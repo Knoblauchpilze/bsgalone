4cb77c9615987bcf02868b0c741aa89d651076b2
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-10-08 19:20:33 +0200

    feat: Coordinator now handles more of the work to create entities and SystemView is less complex

--- src/lib/bsgo/Coordinator.cc
+++ src/lib/bsgo/Coordinator.cc
@@ -1,12 +1,14 @@
 
 #include "Coordinator.hh"
+#include "CircleBox.hh"
 
 namespace bsgo {
 
-Coordinator::Coordinator()
+Coordinator::Coordinator(const Repositories &repos)
   : utils::CoreObject("coordinator")
 {
   setService("bsgo");
+  init(repos);
 }
 
 auto Coordinator::createEntity(const EntityKind &kind) -> Uuid
@@ -72,14 +74,15 @@ auto Coordinator::getVelocity(const Uuid &ent) const -> std::optional<VelocitySh
   return {it->second};
 }
 
-auto Coordinator::getEntityAt(const Eigen::Vector3f &pos) const -> std::optional<Uuid>
+auto Coordinator::getEntityAt(const Eigen::Vector3f &pos,
+                              const std::optional<EntityKind> &filter) const -> std::optional<Uuid>
 {
   std::optional<Uuid> out;
   float best = std::numeric_limits<float>::max();
 
   for (const auto &[uuid, transform] : m_components.transforms)
   {
-    if (transform->contains(pos))
+    if (hasExpectedKind(uuid, filter) && transform->contains(pos))
     {
       const auto d = (pos - transform->position()).norm();
       if (d < best)
@@ -93,14 +96,16 @@ auto Coordinator::getEntityAt(const Eigen::Vector3f &pos) const -> std::optional
   return out;
 }
 
-auto Coordinator::getEntitiesWithin(const IBoundingBox &bbox) const -> std::vector<Uuid>
+auto Coordinator::getEntitiesWithin(const IBoundingBox &bbox,
+                                    const std::optional<EntityKind> &filter) const
+  -> std::vector<Uuid>
 {
   std::vector<Uuid> out;
 
   for (const auto &[uuid, transform] : m_components.transforms)
   {
     /// TODO: We should probably have a 'intersects' method.
-    if (bbox.isInside(transform->position()))
+    if (hasExpectedKind(uuid, filter) && bbox.isInside(transform->position()))
     {
       out.push_back(uuid);
     }
@@ -112,6 +117,63 @@ auto Coordinator::getEntitiesWithin(const IBoundingBox &bbox) const -> std::vect
 void Coordinator::update(float elapsedSeconds)
 {
   m_motionSystem.update(m_components, elapsedSeconds);
+}
+
+void Coordinator::checkRepositories(const Repositories &repos)
+{
+  if (nullptr == repos.system)
+  {
+    throw std::invalid_argument("Expected non null system repository");
+  }
+  if (nullptr == repos.asteroid)
+  {
+    throw std::invalid_argument("Expected non null asteroid repository");
+  }
+  if (nullptr == repos.player)
+  {
+    throw std::invalid_argument("Expected non null player repository");
+  }
+  if (nullptr == repos.ship)
+  {
+    throw std::invalid_argument("Expected non null ship repository");
+  }
+}
+
+void Coordinator::init(const Repositories &repos)
+{
+  checkRepositories(repos);
+
+  const auto systemId = repos.player->findSystemById({});
+
+  const auto ships = repos.system->findShips(systemId);
+  for (const auto &systemShip : ships)
+  {
+    const auto ship = repos.ship->findOneById(systemShip.model);
+    const auto pos  = repos.ship->findPositionById(systemShip.ship);
+
+    auto box = std::make_unique<CircleBox>(pos, ship.radius);
+    createEntity(EntityKind::SHIP, std::move(box), Eigen::Vector3f::Zero());
+  }
+
+  const auto asteroids = repos.system->findAsteroids(systemId);
+  for (const auto &id : asteroids)
+  {
+    const auto asteroid = repos.asteroid->findOneById(id);
+
+    auto box = std::make_unique<CircleBox>(asteroid.position, asteroid.radius);
+    createEntity(EntityKind::ASTEROID, std::move(box));
+  }
+}
+
+bool Coordinator::hasExpectedKind(const Uuid &ent, const std::optional<EntityKind> &kind) const
+{
+  if (!kind)
+  {
+    return true;
+  }
+
+  const auto it = m_entities.find(ent);
+  return it != m_entities.end() && it->second == *kind;
 }
 
 void Coordinator::addTransform(const Uuid &ent, IBoundingBoxPtr bbox)
--- src/lib/views/SystemView.cc
+++ src/lib/views/SystemView.cc
@@ -1,32 +1,12 @@
 
 #include "SystemView.hh"
-#include "CircleBox.hh"
 
 namespace bsgo {
 
-SystemView::SystemView(const CoordinatorShPtr &coordinator, const Repositories &repositories)
+SystemView::SystemView(const CoordinatorShPtr &coordinator)
   : utils::CoreObject("system")
   , m_coordinator(coordinator)
-{
-  setService("view");
-  if (nullptr == repositories.system)
-  {
-    throw std::invalid_argument("Expected non null system repository");
-  }
-  if (nullptr == repositories.asteroid)
-  {
-    throw std::invalid_argument("Expected non null asteroid repository");
-  }
-  if (nullptr == repositories.player)
-  {
-    throw std::invalid_argument("Expected non null player repository");
-  }
-  if (nullptr == repositories.ship)
-  {
-    throw std::invalid_argument("Expected non null ship repository");
-  }
-  init(repositories);
-}
+{}
 
 void SystemView::update(const float elapsedSeconds)
 {
@@ -41,79 +21,27 @@ auto SystemView::getEntity(const Uuid &ent) const -> Entity
 auto SystemView::getEntityAt(const Eigen::Vector3f &pos,
                              const std::optional<EntityKind> &filter) const -> std::optional<Entity>
 {
-  const auto ent = m_coordinator->getEntityAt(pos);
-  if (!ent)
+  const auto id = m_coordinator->getEntityAt(pos, filter);
+  if (!id)
   {
     return {};
   }
-
-  if (filter && !isValidForFilter(*ent, *filter))
-  {
-    return {};
-  }
-
-  return {m_coordinator->getEntity(*ent)};
+  return m_coordinator->getEntity(*id);
 }
 
 auto SystemView::getEntitiesWithin(const IBoundingBox &bbox,
                                    const std::optional<EntityKind> &filter) const
   -> std::vector<Entity>
 {
-  const auto uuids = m_coordinator->getEntitiesWithin(bbox);
+  const auto uuids = m_coordinator->getEntitiesWithin(bbox, filter);
 
   std::vector<Entity> out;
   for (const auto &uuid : uuids)
   {
-    if (!filter || isValidForFilter(uuid, *filter))
-    {
-      out.push_back(m_coordinator->getEntity(uuid));
-    }
+    out.push_back(m_coordinator->getEntity(uuid));
   }
 
   return out;
-}
-
-void SystemView::init(const Repositories &repositories)
-{
-  const auto systemId = repositories.player->findSystemById({});
-
-  const auto ships = repositories.system->findShips(systemId);
-  for (const auto &systemShip : ships)
-  {
-    const auto ship = repositories.ship->findOneById(systemShip.model);
-    const auto pos  = repositories.ship->findPositionById(systemShip.ship);
-
-    auto box       = std::make_unique<CircleBox>(pos, ship.radius);
-    const auto ent = m_coordinator->createEntity(EntityKind::SHIP,
-                                                 std::move(box),
-                                                 Eigen::Vector3f::Zero());
-    m_ships.insert(ent);
-  }
-
-  const auto asteroids = repositories.system->findAsteroids(systemId);
-  for (const auto &id : asteroids)
-  {
-    const auto asteroid = repositories.asteroid->findOneById(id);
-
-    auto box       = std::make_unique<CircleBox>(asteroid.position, asteroid.radius);
-    const auto ent = m_coordinator->createEntity(EntityKind::ASTEROID, std::move(box));
-    m_asteroids.insert(ent);
-  }
-}
-
-bool SystemView::isValidForFilter(const Uuid &ent, const EntityKind &kind) const
-{
-  switch (kind)
-  {
-    case EntityKind::SHIP:
-      return m_ships.contains(ent);
-    case EntityKind::ASTEROID:
-      return m_asteroids.contains(ent);
-    default:
-      warn("Unknown filter " + str(kind));
-      break;
-  }
-  return false;
 }
 
 } // namespace bsgo
