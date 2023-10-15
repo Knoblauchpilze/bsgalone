064bf919ccaecd8ebe9e7f6844389fa584844807
KnoblauchPilze <prolific-programmer@tuta.com>
2023-10-15 14:42:56 +0200

    feat: Acceleration from the ship is now used by the input handler

--- src/lib/bsgo/components/Velocity.hh
+++ src/lib/bsgo/components/Velocity.hh
@@ -3,6 +3,7 @@
 
 #include <eigen3/Eigen/Eigen>
 #include <memory>
+#include <optional>
 
 namespace bsgo {
 
@@ -10,16 +11,21 @@ class Velocity
 {
   public:
   Velocity() = default;
-  Velocity(const Eigen::Vector3f &acceleration, const Eigen::Vector3f &speed);
+  Velocity(const float maxAcceleration, const std::optional<Eigen::Vector3f> &speed = {});
   ~Velocity() = default;
 
   auto acceleration() const noexcept -> Eigen::Vector3f;
   auto speed() const noexcept -> Eigen::Vector3f;
 
-  void accelerate(const Eigen::Vector3f &acc);
+  /// @brief - Sets the maximum acceleration directed in the input direction for
+  /// this component.
+  /// @param direction - the direction to accelerate into.
+  void accelerate(const Eigen::Vector3f &direction);
+
   void update(const float elapsedSeconds);
 
   private:
+  float m_maxAcceleration;
   Eigen::Vector3f m_acceleration{Eigen::Vector3f::Zero()};
   Eigen::Vector3f m_speed{Eigen::Vector3f::Zero()};
 };
