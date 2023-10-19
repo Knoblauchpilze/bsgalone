a3e800cb7196cc9640a9bd4c397aaa62da3f3718
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-10-19 19:49:47 +0200

    feat: Improved implementation

--- src/lib/bsgo/systems/WeaponSystem.cc
+++ src/lib/bsgo/systems/WeaponSystem.cc
@@ -9,28 +9,19 @@ bool isEntityRelevant(const Entity &entity)
   return !entity.weapons.empty() && entity.exists<TargetComponent>();
 }
 
+bool isWeaponAbleToFire(const Entity &ent, const WeaponSlotComponent &weapon)
+{
+  return weapon.active() && weapon.canFire()
+         && (weapon.powerCost() <= ent.access<PowerComponent>().value());
+}
+
 bool isWeaponAbleToFireOn(const Entity &ent, const WeaponSlotComponent &weapon, const Entity &target)
 {
-  if (!weapon.active() || !weapon.canFire())
-  {
-    return false;
-  }
-
   const Eigen::Vector3f pos       = ent.access<TransformComponent>().position();
   const Eigen::Vector3f targetPos = target.access<TransformComponent>().position();
 
   const auto d = (targetPos - pos).norm();
-  if (d >= weapon.range())
-  {
-    return false;
-  }
-
-  if (weapon.powerCost() >= ent.access<PowerComponent>().value())
-  {
-    return false;
-  }
-
-  return true;
+  return d <= weapon.range();
 }
 
 } // namespace
@@ -64,7 +55,7 @@ void WeaponSystem::fireWeaponsForEntity(Entity &ent, Entity &target)
 
 void WeaponSystem::fireWeaponForEntity(Entity &ent, WeaponSlotComponent &weapon, Entity &target)
 {
-  if (!isWeaponAbleToFireOn(ent, weapon, target))
+  if (!isWeaponAbleToFire(ent, weapon) || !isWeaponAbleToFireOn(ent, weapon, target))
   {
     return;
   }
