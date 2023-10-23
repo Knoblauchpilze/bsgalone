58db1949b685df7a43a1d16b4c10f1161c059a27
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-10-23 20:28:53 +0200

    feat: Menu can choose to ignore children when processing inputs

--- src/lib/ui/common/ScreenCommon.hh
+++ src/lib/ui/common/ScreenCommon.hh
@@ -25,4 +25,14 @@ auto generateMenu(const olc::vi2d &pos,
                   bool selectable                            = false,
                   const menu::Layout &layout = menu::Layout::Vertical) -> pge::MenuShPtr;
 
+auto generateSlotMenu(const olc::vi2d &pos,
+                      const olc::vi2d &size,
+                      const std::string &text,
+                      const std::string &name,
+                      olc::Pixel bgColor                         = olc::VERY_DARK_GREEN,
+                      const std::optional<olc::Pixel> &textColor = {},
+                      bool clickable                             = false,
+                      bool selectable                            = false,
+                      const menu::Layout &layout = menu::Layout::Vertical) -> pge::MenuShPtr;
+
 } // namespace pge
