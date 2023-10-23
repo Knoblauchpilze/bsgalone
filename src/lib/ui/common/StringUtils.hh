0829699fce911cb25185cc5eca2a413b045eeb67
KnoblauchPilze <prolific-programmer@tuta.com>
2023-10-23 20:34:08 +0200

    feat: Computer can be activated again and forced sign for computer stats

--- src/lib/ui/common/StringUtils.hh
+++ src/lib/ui/common/StringUtils.hh
@@ -6,5 +6,6 @@
 namespace pge {
 
 auto floatToStr(const float value, const int decimals = 2) -> std::string;
+auto floatToSignedStr(const float value, const int decimals = 2) -> std::string;
 
-}
+} // namespace pge
