6b3fa4153b3eb8c8188187e974125c2084e04c6e
KnoblauchPilze <prolific-programmer@tuta.com>
2023-10-26 22:27:59 +0200

    feat: LootSystem attempts to distribute to all the recipients

--- src/lib/bsgo/components/LootComponent.cc
+++ src/lib/bsgo/components/LootComponent.cc
@@ -23,6 +23,11 @@ void LootComponent::registerRecipient(const Uuid &entity)
   m_recipients.insert(entity);
 }
 
+auto LootComponent::recipients() const -> std::unordered_set<Uuid>
+{
+  return m_recipients;
+}
+
 void LootComponent::update(const float /*elapsedSeconds*/) {}
 
 } // namespace bsgo
