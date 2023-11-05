6819b136ffd5b2629d11b5ad6edfcff7c1294028
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-05 15:25:52 +0100

    fix: Syntax

--- src/lib/bsgo/repositories/SystemRepository.cc
+++ src/lib/bsgo/repositories/SystemRepository.cc
@@ -10,19 +10,19 @@ SystemRepository::SystemRepository()
 auto SystemRepository::findAllAsteroidsBySystem(const Uuid & /*system*/) const
   -> std::unordered_set<Uuid>
 {
-  return {Uuid(0), Uuid(1), Uuid(2), Uuid(3), Uuid(4)};
+  return {Uuid{0}, Uuid{1}, Uuid{2}, Uuid{3}, Uuid{4}};
 }
 
 auto SystemRepository::findAllShipsBySystem(const Uuid & /*system*/) const
   -> std::unordered_set<Uuid>
 {
-  return {Uuid(0)};
+  return {Uuid(0), Uuid{1}};
 }
 
 auto SystemRepository::findAllOutpostsBySystem(const Uuid & /*system*/) const
   -> std::unordered_set<Uuid>
 {
-  return {Uuid(0), Uuid(1)};
+  return {Uuid{0}, Uuid{1}};
 }
 
 } // namespace bsgo
