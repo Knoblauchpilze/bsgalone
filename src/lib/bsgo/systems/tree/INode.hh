7368a76c9d96181f6ffaffd3cc30b12f29730945
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-11-07 22:15:33 +0100

    feat: Added useful links

--- src/lib/bsgo/systems/tree/INode.hh
+++ src/lib/bsgo/systems/tree/INode.hh
@@ -8,14 +8,21 @@
 
 namespace bsgo {
 
+/// https://www.gamedeveloper.com/programming/behavior-trees-for-ai-how-they-work
+/// https://en.wikipedia.org/wiki/Behavior_tree_(artificial_intelligence,_robotics_and_control)
 class INode : public utils::CoreObject
 {
   public:
   INode(const std::string &name);
   ~INode() override = default;
 
+  /// @brief - Process the node and returns the computed state.
+  /// @param data - data to help the processing.
+  /// @return - the state of the node.
   virtual auto tick(const TickData &data) -> NodeState = 0;
-  virtual void reset()                                 = 0;
+
+  /// @brief - Reset the node to its initial state.
+  virtual void reset() = 0;
 };
 
 using INodePtr = std::unique_ptr<INode>;
