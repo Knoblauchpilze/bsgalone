9decc4f641f3c390375683ea4b805b7638ca6f0b
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-10 22:41:23 +0100

    fix: localhost instead of 127.0.0.1 in db scripts

--- database/create_database.sh
+++ database/create_database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # https://stackoverflow.com/questions/8208181/create-postgres-database-using-batch-file-with-template-encoding-owner-and
-psql -h 127.0.0.1 -U bsgadmin postgres -f db_create.sql
+psql -h localhost -U bsgadmin postgres -f db_create.sql
