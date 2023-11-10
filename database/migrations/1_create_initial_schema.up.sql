36e08a5d57adf8ad51226941c86251f4a4ffa27c
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-10 22:41:03 +0100

    fix: Typo

--- database/migrations/1_create_initial_schema.up.sql
+++ database/migrations/1_create_initial_schema.up.sql
@@ -6,8 +6,7 @@ SET search_path = public, pg_catalog;
 SET default_tablespace = '';
 
 -- Register a function to automatically update the `created_at` field
--- of a new object to insert in the DB. We also create a similar one
--- to update the `joined_at` column and a `updated_at` column.
+-- of a new object to insert in the database.
 CREATE OR REPLACE FUNCTION update_created_at() RETURNS TRIGGER AS $$
   BEGIN
     NEW.created_at = now();
