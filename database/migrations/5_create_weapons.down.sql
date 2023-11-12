8da138d7b547951de25c8adbb3f0955a0c596acc
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-11-12 14:43:29 +0100

    feat: Prices for computers and weapons

--- database/migrations/5_create_weapons.down.sql
+++ database/migrations/5_create_weapons.down.sql
@@ -1,4 +1,5 @@
 
 DROP TABLE ship_weapon;
 DROP TABLE player_weapon;
+DROP TABLE weapon_price;
 DROP TABLE weapon;
--- database/migrations/6_create_computers.down.sql
+++ database/migrations/6_create_computers.down.sql
@@ -2,4 +2,5 @@
 DROP TABLE ship_computer;
 DROP TABLE player_computer;
 DROP TABLE computer_allowed_target;
+DROP TABLE computer_price;
 DROP TABLE computer;
