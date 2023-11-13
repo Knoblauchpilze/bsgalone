bb85bc47ceea63ab03b1e0e44371930415191449
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-13 19:13:49 +0100

    feat: Connected ResourceRepository

--- src/lib/bsgo/repositories/ResourceRepository.cc
+++ src/lib/bsgo/repositories/ResourceRepository.cc
@@ -7,22 +7,31 @@ ResourceRepository::ResourceRepository(const DbConnectionShPtr &connection)
   : AbstractRepository("resource", connection)
 {}
 
+namespace {
+constexpr auto SQL_QUERY = "SELECT name FROM resource WHERE id = ";
+
+auto generateSqlQuery(const Uuid &resource) -> std::string
+{
+  return SQL_QUERY + std::to_string(toDbId(resource));
+}
+} // namespace
+
 auto ResourceRepository::findOneById(const Uuid &resource) const -> Resource
 {
+  const auto sql = generateSqlQuery(resource);
+
+  pqxx::nontransaction work(m_connection->connection());
+  pqxx::result rows(work.exec(sql));
+
+  if (rows.size() != 1)
+  {
+    error("Expected to find resource " + str(resource));
+  }
+
   Resource out;
 
-  switch (resource)
-  {
-    case Uuid{0}:
-      out.name = "tylium";
-      break;
-    case Uuid{1}:
-      out.name = "titane";
-      break;
-    default:
-      error("Resource " + str(resource) + " not found");
-      break;
-  }
+  const auto &record = rows[0];
+  out.name           = record[0].as<std::string>();
 
   return out;
 }
