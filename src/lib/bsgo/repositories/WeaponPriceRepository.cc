3094131d8824a908e7c5a6cb8dffa5fc9fa1acba
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-13 19:10:27 +0100

    feat: Connected WeaponPriceRepository to database

--- src/lib/bsgo/repositories/WeaponPriceRepository.cc
+++ src/lib/bsgo/repositories/WeaponPriceRepository.cc
@@ -11,20 +11,33 @@ WeaponPriceRepository::WeaponPriceRepository(const DbConnectionShPtr &connection
   addModule("price");
 }
 
+namespace {
+constexpr auto SQL_QUERY = "SELECT resource, cost FROM weapon_price WHERE weapon = ";
+
+auto generateSqlQuery(const Uuid &weapon) -> std::string
+{
+  return SQL_QUERY + std::to_string(toDbId(weapon));
+}
+} // namespace
+
 auto WeaponPriceRepository::findAllByWeapon(const Uuid &weapon) const
   -> std::unordered_map<Uuid, int>
 {
-  switch (weapon)
+  const auto sql = generateSqlQuery(weapon);
+
+  pqxx::nontransaction work(m_connection->connection());
+  pqxx::result rows(work.exec(sql));
+
+  std::unordered_map<Uuid, int> out;
+  for (const auto record : rows)
   {
-    case Uuid{0}:
-      return {{Uuid{1}, 12}};
-    default:
-      error("Weapon " + str(weapon) + " not found");
-      break;
+    const auto resource = fromDbId(record[0].as<int>());
+    const auto cost     = record[1].as<float>();
+
+    out[resource] = cost;
   }
 
-  // Can't happen because of the error above.
-  return {};
+  return out;
 }
 
 } // namespace bsgo
