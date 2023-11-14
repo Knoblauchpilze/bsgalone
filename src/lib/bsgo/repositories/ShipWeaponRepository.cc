ed28d6ad58be692374dac48376dd968d8b5f1b7d
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-14 19:34:52 +0100

    feat: Connected ShipWeaponRepository to the database

--- src/lib/bsgo/repositories/ShipWeaponRepository.cc
+++ src/lib/bsgo/repositories/ShipWeaponRepository.cc
@@ -9,22 +9,28 @@ ShipWeaponRepository::ShipWeaponRepository(const DbConnectionShPtr &connection)
   addModule("ship");
 }
 
+namespace {
+constexpr auto SQL_QUERY = "SELECT weapon FROM ship_weapon WHERE ship = ";
+auto generateSqlQuery(const Uuid &ship) -> std::string
+{
+  return SQL_QUERY + std::to_string(toDbId(ship));
+}
+} // namespace
+
 auto ShipWeaponRepository::findAllByShip(const Uuid &ship) const -> std::unordered_set<Uuid>
 {
-  switch (ship)
+  const auto sql = generateSqlQuery(ship);
+
+  pqxx::nontransaction work(m_connection->connection());
+  pqxx::result rows(work.exec(sql));
+
+  std::unordered_set<Uuid> out;
+  for (const auto record : rows)
   {
-    case Uuid{0}:
-      return {Uuid(0), Uuid(1)};
-    case Uuid{1}:
-      return {Uuid{4}};
-    case Uuid{2}:
-      return {};
-    default:
-      error("Ship " + str(ship) + " not found");
-      break;
+    out.emplace(fromDbId(record[0].as<int>()));
   }
 
-  return {};
+  return out;
 }
 
 } // namespace bsgo
