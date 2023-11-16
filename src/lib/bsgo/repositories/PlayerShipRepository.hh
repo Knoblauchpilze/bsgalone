543f391c275cac7cd9daa2a140cc933aed292a9b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-11-16 19:11:43 +0100

    feat: PlayerShipRepository is connected to the database but misses slots

--- src/lib/bsgo/repositories/PlayerShipRepository.hh
+++ src/lib/bsgo/repositories/PlayerShipRepository.hh
@@ -3,10 +3,12 @@
 
 #include "AbstractRepository.hh"
 #include "Faction.hh"
+#include "Slot.hh"
 #include "Uuid.hh"
 #include <eigen3/Eigen/Eigen>
 #include <memory>
 #include <optional>
+#include <unordered_set>
 
 namespace bsgo {
 
@@ -30,6 +32,8 @@ struct PlayerShip
 
   float radius{0.5f};
   Eigen::Vector3f position{Eigen::Vector3f::Zero()};
+
+  std::unordered_map<Slot, int> slots{};
 };
 
 class PlayerShipRepository : public AbstractRepository
@@ -39,7 +43,11 @@ class PlayerShipRepository : public AbstractRepository
   ~PlayerShipRepository() override = default;
 
   auto findOneById(const Uuid &ship) const -> PlayerShip;
-  auto findAllByPlayer(const Uuid &player) const -> std::vector<Uuid>;
+  auto findAllByPlayer(const Uuid &player) const -> std::unordered_set<Uuid>;
+
+  private:
+  auto fetchShipBase(const Uuid &ship) const -> PlayerShip;
+  void fetchSlots(const Uuid &ship, PlayerShip &out) const;
 };
 
 using PlayerShipRepositoryShPtr = std::shared_ptr<PlayerShipRepository>;
