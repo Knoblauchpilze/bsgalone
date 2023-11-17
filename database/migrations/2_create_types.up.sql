3441c932ad33ef96df8f3fa5c35ec31ea528c56f
KnoblauchPilze <prolific-programmer@tuta.com>
2023-11-17 15:30:58 +0100

    fix: Formatting

--- database/migrations/2_create_types.up.sql
+++ database/migrations/2_create_types.up.sql
@@ -21,15 +21,15 @@ CREATE TABLE slot (
   PRIMARY KEY (type)
 );
 
-INSERT INTO public.faction ("name") VALUES('cylon');
-INSERT INTO public.faction ("name") VALUES('colonial');
+INSERT INTO public.faction ("name") VALUES ('cylon');
+INSERT INTO public.faction ("name") VALUES ('colonial');
 
-INSERT INTO public.entity ("name") VALUES('ship');
-INSERT INTO public.entity ("name") VALUES('asteroid');
-INSERT INTO public.entity ("name") VALUES('outpost');
+INSERT INTO public.entity ("name") VALUES ('ship');
+INSERT INTO public.entity ("name") VALUES ('asteroid');
+INSERT INTO public.entity ("name") VALUES ('outpost');
 
-INSERT INTO public.resource ("name") VALUES('tylium');
-INSERT INTO public.resource ("name") VALUES('titane');
+INSERT INTO public.resource ("name") VALUES ('tylium');
+INSERT INTO public.resource ("name") VALUES ('titane');
 
-INSERT INTO public.slot ("type") VALUES('weapon');
-INSERT INTO public.slot ("type") VALUES('computer');
+INSERT INTO public.slot ("type") VALUES ('weapon');
+INSERT INTO public.slot ("type") VALUES ('computer');
