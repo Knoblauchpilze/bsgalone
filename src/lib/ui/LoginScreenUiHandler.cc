5f5df499fcdc02ae991840dab5d6c0eb176c3713
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-11-18 15:23:20 +0100

    feat: Login screen ui layout using new UiMenu

--- src/lib/ui/LoginScreenUiHandler.cc
+++ src/lib/ui/LoginScreenUiHandler.cc
@@ -10,76 +10,97 @@ LoginScreenUiHandler::LoginScreenUiHandler()
 
 void LoginScreenUiHandler::initializeMenus(const int width, const int height)
 {
-  const olc::vi2d dims{width, height};
-  m_menu = generateDefaultScreen(dims, olc::DARK_PINK);
-
-  MenuShPtr m = generateScreenOption(dims, "Login", olc::VERY_DARK_PINK, "login", true);
-  m->setSimpleAction([this](Game &g) { g.setScreen(Screen::OUTPOST); });
-  m_menu->addMenu(m);
-
-  m = generateScreenOption(dims, "Quit", olc::VERY_DARK_PINK, "quit", true);
-  m->setSimpleAction([this](Game &g) {
-    g.setScreen(Screen::EXIT);
-    g.terminate();
-  });
-  m_menu->addMenu(m);
-
-  MenuConfig config{.pos    = olc::vi2d{30, 50},
-                    .dims   = olc::vi2d{150, 90},
-                    .layout = MenuLayout::Horizontal};
-  auto bgConfig = bgConfigFromColor(olc::Pixel{255, 255, 128});
-  m_uiMenu      = std::make_unique<UiMenu>(config, bgConfig);
-
-  // Horizontal
-  config.layout     = MenuLayout::Vertical;
-  bgConfig          = bgConfigFromColor(olc::DARK_BROWN);
-  auto menuVertical = std::make_unique<UiMenu>(config, bgConfig);
-
-  bgConfig        = bgConfigFromColor(olc::DARK_CYAN);
-  auto textConfig = textConfigFromColor("menu1", olc::DARK_COBALT_BLUE, TextAlignment::LEFT);
-  auto menu       = std::make_unique<UiMenu>(config, bgConfig, textConfig);
-  menuVertical->addMenu(std::move(menu));
-
-  bgConfig   = bgConfigFromColor(olc::DARK_ORANGE);
-  textConfig = textConfigFromColor("menu2", olc::DARK_GREEN, TextAlignment::CENTER);
-  menu       = std::make_unique<UiMenu>(config, bgConfig, textConfig);
-  menuVertical->addMenu(std::move(menu));
-
-  bgConfig   = bgConfigFromColor(olc::DARK_PINK);
-  textConfig = textConfigFromColor("menu3", olc::DARK_GREY, TextAlignment::RIGHT);
-  menu       = std::make_unique<UiMenu>(config, bgConfig, textConfig);
-  menuVertical->addMenu(std::move(menu));
-
-  m_uiMenu->addMenu(std::move(menuVertical));
-
-  bgConfig = bgConfigFromColor(olc::BLACK);
-  menu     = std::make_unique<UiMenu>(config, bgConfig);
-  m_uiMenu->addMenu(std::move(menu));
-
-  bgConfig   = bgConfigFromColor(olc::DARK_PURPLE);
-  textConfig = textConfigFromColor("hihi", olc::DARK_RED, TextAlignment::RIGHT);
-  menu       = std::make_unique<UiMenu>(config, bgConfig, textConfig);
-  m_uiMenu->addMenu(std::move(menu));
+  generateLoginMenu(width, height);
+  generateLoginButton(width, height);
+  generateQuitMenu(width, height);
 }
 
 bool LoginScreenUiHandler::processUserInput(UserInputData &inputData)
 {
-  const auto res = m_uiMenu->processUserInput(inputData);
-  if (res)
+  if (m_quitButton->processUserInput(inputData))
   {
-    return res;
+    return true;
+  }
+  if (m_loginButton->processUserInput(inputData))
+  {
+    return true;
   }
 
-  const auto out = m_menu->processUserInput(inputData.controls, inputData.actions);
-  return out.relevant;
+  return m_loginPanel->processUserInput(inputData);
 }
 
 void LoginScreenUiHandler::render(SpriteRenderer &engine) const
 {
-  m_menu->render(engine.getRenderer());
-  m_uiMenu->render(engine.getRenderer());
+  m_loginPanel->render(engine.getRenderer());
+  m_loginButton->render(engine.getRenderer());
+  m_quitButton->render(engine.getRenderer());
 }
 
 void LoginScreenUiHandler::updateUi() {}
+
+void LoginScreenUiHandler::generateLoginMenu(const int width, const int /*height*/)
+{
+  const olc::vi2d loginDimsPixels{400, 400};
+  constexpr auto LOGIN_PANEL_Y_PIXELS = 30;
+  const olc::vi2d loginPos{(width - loginDimsPixels.x) / 2, LOGIN_PANEL_Y_PIXELS};
+
+  MenuConfig config{.pos = loginPos, .dims = loginDimsPixels, .highligtable = false};
+  auto bg      = bgConfigFromColor(transparent(olc::WHITE));
+  m_loginPanel = std::make_unique<UiMenu>(config, bg);
+
+  bg         = bgConfigFromColor(transparent(olc::DARK_BLUE, alpha::SemiOpaque));
+  auto text  = textConfigFromColor("Name:", olc::DARK_GREY, TextAlignment::LEFT);
+  auto label = std::make_unique<UiMenu>(config, bg, text);
+  m_loginPanel->addMenu(std::move(label));
+
+  bg    = bgConfigFromColor(transparent(olc::WHITE, alpha::SemiOpaque));
+  text  = textConfigFromColor("haha", olc::BLACK, TextAlignment::LEFT);
+  label = std::make_unique<UiMenu>(config, bg, text);
+  m_loginPanel->addMenu(std::move(label));
+
+  bg    = bgConfigFromColor(transparent(olc::DARK_BLUE, alpha::SemiOpaque));
+  text  = textConfigFromColor("Password:", olc::DARK_GREY, TextAlignment::LEFT);
+  label = std::make_unique<UiMenu>(config, bg, text);
+  m_loginPanel->addMenu(std::move(label));
+
+  bg    = bgConfigFromColor(transparent(olc::WHITE, alpha::SemiOpaque));
+  text  = textConfigFromColor("pwd", olc::BLACK, TextAlignment::LEFT);
+  label = std::make_unique<UiMenu>(config, bg, text);
+  m_loginPanel->addMenu(std::move(label));
+}
+
+void LoginScreenUiHandler::generateLoginButton(const int width, const int height)
+{
+  constexpr auto REASONABLE_GAP_SIZE = 60;
+
+  const olc::vi2d loginButtonDimsPixels{200, 60};
+  const olc::vi2d loginButtonPos{(width - loginButtonDimsPixels.x) / 2,
+                                 height - REASONABLE_GAP_SIZE - loginButtonDimsPixels.y};
+
+  const MenuConfig config{.pos = loginButtonPos, .dims = loginButtonDimsPixels};
+
+  const auto bg   = bgConfigFromColor(olc::DARK_COBALT_BLUE);
+  const auto text = textConfigFromColor("AU JEU!!", olc::WHITE);
+  m_loginButton   = std::make_unique<UiMenu>(config, bg, text);
+}
+
+void LoginScreenUiHandler::generateQuitMenu(const int width, const int height)
+{
+  constexpr auto REASONABLE_GAP_SIZE = 20;
+  const olc::vi2d quitButtonDimsPixels{100, 50};
+  const olc::vi2d quitButtonPos{width - REASONABLE_GAP_SIZE - quitButtonDimsPixels.x,
+                                height - REASONABLE_GAP_SIZE - quitButtonDimsPixels.y};
+
+  const MenuConfig config{.pos               = quitButtonPos,
+                          .dims              = quitButtonDimsPixels,
+                          .gameClickCallBack = [](Game &g) {
+                            g.setScreen(Screen::EXIT);
+                            g.terminate();
+                          }};
+
+  const auto bg   = bgConfigFromColor(olc::VERY_DARK_GREY);
+  const auto text = textConfigFromColor("Quit", olc::WHITE);
+  m_quitButton    = std::make_unique<UiMenu>(config, bg, text);
+}
 
 } // namespace pge
