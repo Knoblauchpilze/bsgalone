4dfc56a0104fe1e87a6a470c1592dc8263e5b730
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-11-21 17:09:10 +0100

    feat: Added methods to fetch System in the repository

--- src/lib/bsgo/repositories/SystemRepository.cc
+++ src/lib/bsgo/repositories/SystemRepository.cc
@@ -8,6 +8,14 @@ SystemRepository::SystemRepository(const DbConnectionShPtr &connection)
 {}
 
 namespace {
+constexpr auto SQL_QUERY_ALL = "SELECT id FROM system";
+
+constexpr auto SQL_QUERY_SYSTEM = "SELECT name, x_pos, y_pos, z_pos FROM system WHERE id = ";
+auto generateSystemSqlQuery(const Uuid &system) -> std::string
+{
+  return SQL_QUERY_SYSTEM + std::to_string(toDbId(system));
+}
+
 constexpr auto SQL_QUERY_ASTEROID = "SELECT id FROM asteroid WHERE system = ";
 auto generateAsteroidSqlQuery(const Uuid &system) -> std::string
 {
@@ -26,6 +34,45 @@ auto generateOutpostSqlQuery(const Uuid &system) -> std::string
   return SQL_QUERY_OUTPOST + std::to_string(toDbId(system));
 }
 } // namespace
+
+auto SystemRepository::findAll() const -> std::unordered_set<Uuid>
+{
+  pqxx::nontransaction work(m_connection->connection());
+  pqxx::result rows(work.exec(SQL_QUERY_ALL));
+
+  std::unordered_set<Uuid> out;
+  for (const auto record : rows)
+  {
+    out.emplace(fromDbId(record[0].as<int>()));
+  }
+
+  return out;
+}
+
+auto SystemRepository::findOneById(const Uuid &system) const -> System
+{
+  const auto sql = generateSystemSqlQuery(system);
+
+  pqxx::nontransaction work(m_connection->connection());
+  pqxx::result rows(work.exec(sql));
+
+  if (rows.size() != 1)
+  {
+    error("Expected to find only one system with id " + str(system));
+  }
+
+  System out{};
+
+  const auto &record = rows[0];
+  out.name           = record[0].as<std::string>();
+
+  const auto x = record[1].as<float>();
+  const auto y = record[2].as<float>();
+  const auto z = record[3].as<float>();
+  out.position = Eigen::Vector3f(x, y, z);
+
+  return out;
+}
 
 auto SystemRepository::findAllAsteroidsBySystem(const Uuid &system) const
   -> std::unordered_set<Uuid>
--- src/lib/bsgo/repositories/SystemRepository.hh
+++ src/lib/bsgo/repositories/SystemRepository.hh
@@ -3,16 +3,26 @@
 
 #include "AbstractRepository.hh"
 #include "Uuid.hh"
+#include <eigen3/Eigen/Eigen>
 #include <memory>
 #include <unordered_set>
 
 namespace bsgo {
+
+struct System
+{
+  std::string name{};
+  Eigen::Vector3f position{};
+};
 
 class SystemRepository : public AbstractRepository
 {
   public:
   SystemRepository(const DbConnectionShPtr &connection);
   ~SystemRepository() override = default;
+
+  auto findAll() const -> std::unordered_set<Uuid>;
+  auto findOneById(const Uuid &system) const -> System;
 
   auto findAllAsteroidsBySystem(const Uuid &system) const -> std::unordered_set<Uuid>;
   auto findAllShipsBySystem(const Uuid &system) const -> std::unordered_set<Uuid>;
