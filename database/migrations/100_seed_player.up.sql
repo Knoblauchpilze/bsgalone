dfc02d436284996cae3f4400f86fd3a0394dcc6d
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-01 21:52:41 +0100

    feat: More titane for grouton

--- database/migrations/100_seed_player.up.sql
+++ database/migrations/100_seed_player.up.sql
@@ -14,5 +14,5 @@ INSERT INTO public.player_resource ("player", "resource", "amount")
   VALUES (
     (SELECT id FROM player WHERE name = 'grouton'),
     (SELECT id FROM resource WHERE name = 'titane'),
-    1.2
+    1017.2
   );
