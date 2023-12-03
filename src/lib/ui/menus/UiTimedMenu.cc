a0f94624db30ecc7ea5040125b910556932acbf2
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-03 16:51:10 +0100

    feat: Timed menu can now fade out

--- src/lib/ui/menus/UiTimedMenu.cc
+++ src/lib/ui/menus/UiTimedMenu.cc
@@ -1,12 +1,16 @@
 
 #include "UiTimedMenu.hh"
+#include "UiTextMenu.hh"
 
 namespace pge {
 
-UiTimedMenu::UiTimedMenu(const utils::Duration &duration, UiMenuPtr menu)
+UiTimedMenu::UiTimedMenu(UiMenuPtr menu)
+  : UiTimedMenu(TimedMenuConfig{}, std::move(menu))
+{}
+
+UiTimedMenu::UiTimedMenu(const TimedMenuConfig &config, UiMenuPtr menu)
   : utils::CoreObject("timed")
   , m_menu(std::move(menu))
-  , m_duration(duration)
 {
   setService("menu");
 
@@ -14,29 +18,83 @@ UiTimedMenu::UiTimedMenu(const utils::Duration &duration, UiMenuPtr menu)
   {
     throw std::invalid_argument("Expected non null menu");
   }
+
+  initializeFromConfig(config);
+  m_menu->setVisible(false);
 }
 
 void UiTimedMenu::trigger()
 {
-  m_elapsedSinceLastTrigger = utils::Duration(0);
   m_menu->setVisible(true);
+  updateOpacity(1.0f);
+
+  m_lastTrigger = utils::now();
+  if (m_fadeOutDuration)
+  {
+    m_fadeOutStartTime = *m_lastTrigger + m_duration - *m_fadeOutDuration;
+  }
 }
 
-void UiTimedMenu::update(const float elapsedSeconds)
+void UiTimedMenu::update()
 {
-  if (!m_elapsedSinceLastTrigger)
+  if (!m_lastTrigger)
   {
     return;
   }
 
-  constexpr auto MILLISECONDS_IN_A_SECONDS = 1000;
-  (*m_elapsedSinceLastTrigger) += utils::Milliseconds(
-    static_cast<int>(elapsedSeconds * MILLISECONDS_IN_A_SECONDS));
+  const auto now     = utils::now();
+  const auto elapsed = now - *m_lastTrigger;
+  handleFadeOut(now);
 
-  if (*m_elapsedSinceLastTrigger >= m_duration)
+  if (elapsed >= m_duration)
   {
-    m_elapsedSinceLastTrigger.reset();
+    m_lastTrigger.reset();
+    m_fadeOutStartTime.reset();
+
     m_menu->setVisible(false);
+  }
+}
+
+void UiTimedMenu::render(olc::PixelGameEngine *pge) const
+{
+  m_menu->render(pge);
+}
+
+void UiTimedMenu::initializeFromConfig(const TimedMenuConfig &config)
+{
+  m_duration = config.duration;
+
+  if (!config.fadeOut)
+  {
+    return;
+  }
+
+  m_fadeOutDuration = config.fadeOutDuration;
+}
+
+void UiTimedMenu::handleFadeOut(const utils::TimeStamp &now)
+{
+  if (!m_fadeOutStartTime || now < *m_fadeOutStartTime)
+  {
+    return;
+  }
+
+  const auto fadeOutElapsedTimeAsFloat = std::chrono::duration<float, std::milli>(
+    now - *m_fadeOutStartTime);
+  const auto perc = 1.0f - fadeOutElapsedTimeAsFloat / *m_fadeOutDuration;
+  updateOpacity(perc);
+}
+
+void UiTimedMenu::updateOpacity(const float perc)
+{
+  const auto bg = opacifyFromPercentage(m_menu->getBgColor(), perc);
+  m_menu->updateBgColor(bg);
+
+  const auto maybeTextMenu = dynamic_cast<UiTextMenu *>(m_menu.get());
+  if (nullptr != maybeTextMenu)
+  {
+    const auto text = opacifyFromPercentage(maybeTextMenu->getTextColor(), perc);
+    maybeTextMenu->updateTextColor(text);
   }
 }
 
