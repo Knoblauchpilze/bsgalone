aa78c412233d21b009b72884116b0b9704cff75f
KnoblauchPilze <prolific-programmer@tuta.com>
2023-12-03 16:51:10 +0100

    feat: Timed menu can now fade out

--- src/lib/ui/menus/UiTimedMenu.hh
+++ src/lib/ui/menus/UiTimedMenu.hh
@@ -4,22 +4,43 @@
 #include "UiMenu.hh"
 #include <core_utils/CoreObject.hh>
 #include <core_utils/TimeUtils.hh>
+#include <memory>
 
 namespace pge {
 
+struct TimedMenuConfig
+{
+  utils::Duration duration{utils::Milliseconds(1500)};
+
+  bool fadeOut{true};
+  utils::Duration fadeOutDuration{utils::Milliseconds(1000)};
+};
+
 class UiTimedMenu : public utils::CoreObject
 {
-  UiTimedMenu(const utils::Duration &duration, UiMenuPtr menu);
+  public:
+  UiTimedMenu(UiMenuPtr menu);
+  UiTimedMenu(const TimedMenuConfig &config, UiMenuPtr menu);
   ~UiTimedMenu() override = default;
 
   void trigger();
-  void update(const float elapsedSeconds);
+  void update();
+
+  void render(olc::PixelGameEngine *pge) const;
 
   private:
   UiMenuPtr m_menu{};
   utils::Duration m_duration{};
+  std::optional<utils::Duration> m_fadeOutDuration{};
 
-  std::optional<utils::Duration> m_elapsedSinceLastTrigger{};
+  std::optional<utils::TimeStamp> m_lastTrigger{};
+  std::optional<utils::TimeStamp> m_fadeOutStartTime{};
+
+  void initializeFromConfig(const TimedMenuConfig &config);
+  void handleFadeOut(const utils::TimeStamp &now);
+  void updateOpacity(const float perc);
 };
+
+using UiTimedMenuPtr = std::unique_ptr<UiTimedMenu>;
 
 } // namespace pge
