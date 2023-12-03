af4465419b18352a7c302137d3205f883d452be1
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-03 15:46:38 +0100

    feat: Removed the Menu references from the ScreenCommon

--- src/lib/ui/common/ScreenCommon.hh
+++ src/lib/ui/common/ScreenCommon.hh
@@ -1,40 +1,11 @@
 
 #pragma once
 
-#include "Menu.hh"
 #include "UiMenu.hh"
 #include <optional>
 #include <string>
 
 namespace pge {
-
-auto generateDefaultScreen(const olc::vi2d &dims, const olc::Pixel &color) -> MenuShPtr;
-
-auto generateScreenOption(const olc::vi2d &dims,
-                          const std::string &text,
-                          const olc::Pixel &bgColor,
-                          const std::string &name,
-                          bool selectable) -> MenuShPtr;
-
-auto generateMenu(const olc::vi2d &pos,
-                  const olc::vi2d &size,
-                  const std::string &text,
-                  const std::string &name,
-                  olc::Pixel bgColor                         = olc::VERY_DARK_GREEN,
-                  const std::optional<olc::Pixel> &textColor = {},
-                  bool clickable                             = false,
-                  bool selectable                            = false,
-                  const menu::Layout &layout = menu::Layout::Vertical) -> pge::MenuShPtr;
-
-auto generateSlotMenu(const olc::vi2d &pos,
-                      const olc::vi2d &size,
-                      const std::string &text,
-                      const std::string &name,
-                      olc::Pixel bgColor                         = olc::VERY_DARK_GREEN,
-                      const std::optional<olc::Pixel> &textColor = {},
-                      bool clickable                             = false,
-                      bool selectable                            = false,
-                      const menu::Layout &layout = menu::Layout::Vertical) -> pge::MenuShPtr;
 
 auto generateBlankHorizontalMenu(const std::optional<olc::vi2d> &dims = {}) -> UiMenuPtr;
 auto generateBlankHorizontalMenu(const olc::vi2d &pos, const std::optional<olc::vi2d> &dims = {})
