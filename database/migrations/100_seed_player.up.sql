43878ef2bcfdc1853af89df3898152fd527602b6
KnoblauchPilze <prolific-programmer@tuta.com>
2023-12-05 22:53:12 +0100

    fix: Missing faction in player migration

--- database/migrations/100_seed_player.up.sql
+++ database/migrations/100_seed_player.up.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO public.player ("name", "password")
-  VALUES ('grouton', '123456');
-INSERT INTO public.player ("name", "password")
-  VALUES ('knoppgrunt', '654321');
+INSERT INTO public.player ("name", "password", "faction")
+  VALUES ('grouton', '123456', 'cylon');
+INSERT INTO public.player ("name", "password", "faction")
+  VALUES ('knoppgrunt', '654321', 'colonial');
 
 INSERT INTO public.player_resource ("player", "resource", "amount")
   VALUES (
