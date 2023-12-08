d980427c4f9a48dcdf0fc194e280c79a627111e5
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-08 16:13:09 +0100

    feat: Skeleton to process purchase requests, missing the db implementation

--- src/lib/ui/outpost/ShopUiHandler.cc
+++ src/lib/ui/outpost/ShopUiHandler.cc
@@ -223,8 +223,11 @@ auto ShopUiHandler::generateBuySection(const int itemId) -> UiMenuPtr
 void ShopUiHandler::onPurchaseRequest(const int itemId)
 {
   const auto &purchase = m_itemsData.at(itemId);
-  /// TODO: Handle the purchase request.
-  warn("should buy " + bsgo::str(purchase.itemId) + " with type " + bsgo::str(purchase.itemType));
+  if (m_shopView->tryPurchase(purchase.itemId, purchase.itemType))
+  {
+    warn("Failed to buy buy " + bsgo::str(purchase.itemId) + " with type "
+         + bsgo::str(purchase.itemType));
+  }
 }
 
 } // namespace pge
