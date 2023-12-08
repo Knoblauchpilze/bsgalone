12722ac6f0e35e8d5aabaa90d3c4167b58697a05
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-08 16:56:52 +0100

    fix: Formatting

--- database/migrations/8_player_creation.up.sql
+++ database/migrations/8_player_creation.up.sql
@@ -9,42 +9,42 @@ CREATE PROCEDURE player_signup (
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    -- insert in player table
-    INSERT INTO player("name", "password", "faction")
-      VALUES (player_name, player_password, player_faction);
+  -- insert in player table
+  INSERT INTO player("name", "password", "faction")
+    VALUES (player_name, player_password, player_faction);
 
-    -- register some resources for the new player
-    INSERT INTO player_resource ("player", "resource", "amount")
-      VALUES
-      (
-        (SELECT id FROM player WHERE name = player_name),
-        (SELECT id FROM resource WHERE name = 'tylium'),
-        10000.0
-      ),
-      (
-        (SELECT id FROM player WHERE name = player_name),
-        (SELECT id FROM resource WHERE name = 'titane'),
-        10000.0
-      );
+  -- register some resources for the new player
+  INSERT INTO player_resource ("player", "resource", "amount")
+    VALUES
+    (
+      (SELECT id FROM player WHERE name = player_name),
+      (SELECT id FROM resource WHERE name = 'tylium'),
+      10000.0
+    ),
+    (
+      (SELECT id FROM player WHERE name = player_name),
+      (SELECT id FROM resource WHERE name = 'titane'),
+      10000.0
+    );
 
-    -- create the first player ship
-    INSERT INTO player_ship ("ship", "player", "active", "hull_points", "power_points", "x_pos", "y_pos", "z_pos")
-      VALUES (
-        (SELECT id FROM ship WHERE starting_ship = true AND faction = player_faction),
-        (SELECT id FROM player WHERE name = player_name),
-        true,
-        (SELECT max_hull_points FROM ship WHERE starting_ship = true AND faction = player_faction),
-        (SELECT max_power_points FROM ship WHERE starting_ship = true AND faction = player_faction),
-        0.0,
-        0.0,
-        0.0
-      );
+  -- create the first player ship
+  INSERT INTO player_ship ("ship", "player", "active", "hull_points", "power_points", "x_pos", "y_pos", "z_pos")
+    VALUES (
+      (SELECT id FROM ship WHERE starting_ship = true AND faction = player_faction),
+      (SELECT id FROM player WHERE name = player_name),
+      true,
+      (SELECT max_hull_points FROM ship WHERE starting_ship = true AND faction = player_faction),
+      (SELECT max_power_points FROM ship WHERE starting_ship = true AND faction = player_faction),
+      0.0,
+      0.0,
+      0.0
+    );
 
-    -- spawn the ship in the starting system
-    INSERT INTO public.ship_system ("ship", "system")
-      VALUES (
-        (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = player_name),
-        (SELECT system FROM starting_system WHERE faction = player_faction)
-      );
+  -- spawn the ship in the starting system
+  INSERT INTO public.ship_system ("ship", "system")
+    VALUES (
+      (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = player_name),
+      (SELECT system FROM starting_system WHERE faction = player_faction)
+    );
 END
 $$;
