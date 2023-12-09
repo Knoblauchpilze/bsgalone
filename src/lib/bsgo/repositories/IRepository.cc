796c00e0019db3bd2a40de460add67db66ef4d86
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-09 09:45:19 +0100

    feat: IRepository has a virtual initialization method which aims at preparing the sql queries

--- src/lib/bsgo/repositories/IRepository.cc
+++ src/lib/bsgo/repositories/IRepository.cc
@@ -9,4 +9,9 @@ IRepository::IRepository(const std::string &name)
   setService("repository");
 }
 
+void IRepository::initialize()
+{
+  /// TODO: Remove this.
+}
+
 } // namespace bsgo
--- src/lib/bsgo/repositories/IRepository.hh
+++ src/lib/bsgo/repositories/IRepository.hh
@@ -10,6 +10,11 @@ class IRepository : public utils::CoreObject
   public:
   IRepository(const std::string &name);
   ~IRepository() override = default;
+
+  /// @brief - To reimplement to prepare the queries used by this repository.
+  /// This is useful to make the subsequent executions of the queries faster.
+  /// See: https://libpqxx.readthedocs.io/en/6.4/a01480.html
+  virtual void initialize();
 };
 
 } // namespace bsgo
