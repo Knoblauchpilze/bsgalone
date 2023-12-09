887ee3b2c74dce66b6382f7fd04a64211a83b6a3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-09 09:51:23 +0100

    feat: AsteroidLootRepository prepares its query

--- src/lib/bsgo/repositories/AsteroidLootRepository.cc
+++ src/lib/bsgo/repositories/AsteroidLootRepository.cc
@@ -10,30 +10,26 @@ AsteroidLootRepository::AsteroidLootRepository(const DbConnectionShPtr &connecti
 }
 
 namespace {
-constexpr auto SQL_QUERY = "SELECT resource, amount FROM asteroid_loot WHERE asteroid = ";
-auto generateSqlQuery(const Uuid &asteroid) -> std::string
-{
-  return SQL_QUERY + std::to_string(toDbId(asteroid));
-}
+constexpr auto FIND_ONE_QUERY_NAME = "asteroid_loot_find_one";
+constexpr auto FIND_ONE_QUERY = "SELECT resource, amount FROM asteroid_loot WHERE asteroid = $1";
 } // namespace
+
+void AsteroidLootRepository::initialize()
+{
+  m_connection->prepare(FIND_ONE_QUERY_NAME, FIND_ONE_QUERY);
+}
+
+namespace {} // namespace
 
 auto AsteroidLootRepository::findOneById(const Uuid &asteroid) const -> AsteroidLoot
 {
-  const auto sql = generateSqlQuery(asteroid);
-
-  pqxx::nontransaction work(m_connection->connection());
-  const auto rows(work.exec(sql));
-
-  if (rows.size() != 1)
-  {
-    error("Expected to find loot for asteroid " + str(asteroid));
-  }
+  auto work         = m_connection->nonTransaction();
+  const auto record = work.exec_prepared1(FIND_ONE_QUERY_NAME, toDbId(asteroid));
 
   AsteroidLoot out;
 
-  const auto &record = rows[0];
-  out.resource       = fromDbId(record[0].as<int>());
-  out.amount         = record[1].as<float>();
+  out.resource = fromDbId(record[0].as<int>());
+  out.amount   = record[1].as<float>();
 
   return out;
 }
