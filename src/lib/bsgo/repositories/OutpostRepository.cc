71d93880287af081baddea9e7b7d4289fa089919
KnoblauchPilze <prolific-programmer@tuta.com>
2023-12-09 10:11:52 +0100

    feat: OutpostRepository prepares its queries

--- src/lib/bsgo/repositories/OutpostRepository.cc
+++ src/lib/bsgo/repositories/OutpostRepository.cc
@@ -8,29 +8,23 @@ OutpostRepository::OutpostRepository(const DbConnectionShPtr &connection)
 {}
 
 namespace {
-constexpr auto SQL_QUERY
-  = "SELECT faction, max_hull_points, hull_points_regen, max_power_points, power_points_regen, radius FROM outpost WHERE id = ";
-auto generateSqlQuery(const Uuid &ship) -> std::string
-{
-  return SQL_QUERY + std::to_string(toDbId(ship));
-}
+constexpr auto FIND_ONE_QUERY_NAME = "outpost_find_one";
+constexpr auto FIND_ONE_QUERY
+  = "SELECT faction, max_hull_points, hull_points_regen, max_power_points, power_points_regen, radius FROM outpost WHERE id = $1";
 } // namespace
+
+void OutpostRepository::initialize()
+{
+  m_connection->prepare(FIND_ONE_QUERY_NAME, FIND_ONE_QUERY);
+}
 
 auto OutpostRepository::findOneById(const Uuid &outpost) const -> Outpost
 {
-  const auto sql = generateSqlQuery(outpost);
-
-  pqxx::nontransaction work(m_connection->connection());
-  const auto rows(work.exec(sql));
-
-  if (rows.size() != 1)
-  {
-    error("Expected to find only one outpost with id " + str(outpost));
-  }
+  auto work         = m_connection->nonTransaction();
+  const auto record = work.exec_prepared1(FIND_ONE_QUERY_NAME, toDbId(outpost));
 
   Outpost out;
 
-  const auto &record  = rows[0];
   out.faction         = fromDbFaction(record[0].as<std::string>());
   out.maxHullPoints   = record[1].as<float>();
   out.hullPointsRegen = record[2].as<float>();
