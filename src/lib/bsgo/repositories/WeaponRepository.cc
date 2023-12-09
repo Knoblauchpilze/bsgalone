b38566d2ad0045950f485fb9656fe77e17ae2e2a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-09 11:58:34 +0100

    feat: WeaponRepository and WeaponPriceRepository prepare their queries

--- src/lib/bsgo/repositories/WeaponRepository.cc
+++ src/lib/bsgo/repositories/WeaponRepository.cc
@@ -9,15 +9,53 @@ WeaponRepository::WeaponRepository(const DbConnectionShPtr &connection)
 {}
 
 namespace {
-constexpr auto SQL_QUERY_ALL = "SELECT id FROM weapon";
+constexpr auto FIND_ALL_QUERY_NAME = "weapon_find_all";
+constexpr auto FIND_ALL_QUERY      = "SELECT id FROM weapon";
 
-constexpr auto SQL_QUERY
-  = "SELECT name, min_damage, max_damage, power_cost, range, reload_time_ms FROM weapon WHERE id = ";
-auto generateSqlQuery(const Uuid &weapon) -> std::string
+constexpr auto FIND_ONE_QUERY_NAME = "weapon_find_one";
+constexpr auto FIND_ONE_QUERY
+  = "SELECT name, min_damage, max_damage, power_cost, range, reload_time_ms FROM weapon WHERE id = $1";
+} // namespace
+
+void WeaponRepository::initialize()
 {
-  return SQL_QUERY + std::to_string(toDbId(weapon));
+  m_connection->prepare(FIND_ALL_QUERY_NAME, FIND_ALL_QUERY);
+  m_connection->prepare(FIND_ONE_QUERY_NAME, FIND_ONE_QUERY);
 }
 
+auto WeaponRepository::findAll() const -> std::unordered_set<Uuid>
+{
+  auto work       = m_connection->nonTransaction();
+  const auto rows = work.exec_prepared(FIND_ALL_QUERY_NAME);
+
+  std::unordered_set<Uuid> out;
+  for (const auto record : rows)
+  {
+    out.emplace(fromDbId(record[0].as<int>()));
+  }
+
+  return out;
+}
+
+auto WeaponRepository::findOneById(const Uuid &weapon) const -> Weapon
+{
+  auto work         = m_connection->nonTransaction();
+  const auto record = work.exec_prepared1(FIND_ONE_QUERY_NAME, toDbId(weapon));
+
+  Weapon out{};
+
+  out.id         = weapon;
+  out.name       = record[0].as<std::string>();
+  out.minDamage  = record[1].as<float>();
+  out.maxDamage  = record[2].as<float>();
+  out.powerCost  = record[3].as<float>();
+  out.range      = record[4].as<float>();
+  out.reloadTime = utils::Milliseconds(record[5].as<int>());
+
+  return out;
+}
+
+namespace {
 constexpr auto SQL_PURCHASE_PROCEDURE_NAME = "player_buy_weapon";
 auto generatePurchaseSqlQuery(const Uuid &player, const Uuid &weapon) -> std::string
 {
@@ -33,47 +71,6 @@ auto generatePurchaseSqlQuery(const Uuid &player, const Uuid &weapon) -> std::st
   return out;
 }
 } // namespace
-
-auto WeaponRepository::findAll() const -> std::unordered_set<Uuid>
-{
-  pqxx::nontransaction work(m_connection->connection());
-  const auto rows(work.exec(SQL_QUERY_ALL));
-
-  std::unordered_set<Uuid> out;
-  for (const auto record : rows)
-  {
-    out.emplace(fromDbId(record[0].as<int>()));
-  }
-
-  return out;
-}
-
-auto WeaponRepository::findOneById(const Uuid &weapon) const -> Weapon
-{
-  const auto sql = generateSqlQuery(weapon);
-
-  pqxx::nontransaction work(m_connection->connection());
-  const auto rows(work.exec(sql));
-
-  if (rows.size() != 1)
-  {
-    error("Expected to find only one weapon with id " + str(weapon));
-  }
-
-  Weapon out{};
-
-  const auto &record = rows[0];
-  out.id             = weapon;
-  out.name           = record[0].as<std::string>();
-  out.minDamage      = record[1].as<float>();
-  out.maxDamage      = record[2].as<float>();
-  out.powerCost      = record[3].as<float>();
-  out.range          = record[4].as<float>();
-  out.reloadTime     = utils::Milliseconds(record[5].as<int>());
-
-  return out;
-}
-
 bool WeaponRepository::saveForPlayer(const Uuid &player, const Uuid &weapon)
 {
   const auto sql = generatePurchaseSqlQuery(player, weapon);
