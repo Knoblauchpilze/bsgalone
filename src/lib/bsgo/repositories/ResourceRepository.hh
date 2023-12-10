8e537adc08735c9150d4c965005529d623c66d18
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-10 17:47:42 +0100

    feat: ResourceRepository allows to fetch a resource by name

--- src/lib/bsgo/repositories/ResourceRepository.hh
+++ src/lib/bsgo/repositories/ResourceRepository.hh
@@ -23,6 +23,7 @@ class ResourceRepository : public AbstractRepository
   void initialize() override;
 
   auto findOneById(const Uuid &resource) const -> Resource;
+  auto findOneByName(const std::string &name) const -> std::optional<Resource>;
 };
 
 using ResourceRepositoryShPtr = std::shared_ptr<ResourceRepository>;
