cb13a6a51821cb26c59588c688009e064842f6f8
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-12-14 22:28:57 +0100

    feat: Utility method to create a text config

--- src/lib/ui/menus/TextConfig.cc
+++ src/lib/ui/menus/TextConfig.cc
@@ -8,10 +8,15 @@ auto textConfigFromColor(const std::string &text,
                          const olc::Pixel &color,
                          const TextAlignment &align) -> TextConfig
 {
-  return TextConfig{.text   = text,
-                    .color  = color,
-                    .hColor = findHighlightColor(color),
-                    .align  = align};
+  return textConfigFromColor(text, color, findHighlightColor(color), align);
+}
+
+auto textConfigFromColor(const std::string &text,
+                         const olc::Pixel &color,
+                         const olc::Pixel &hColor,
+                         const TextAlignment &align) -> TextConfig
+{
+  return TextConfig{.text = text, .color = color, .hColor = hColor, .align = align};
 }
 
 } // namespace pge
