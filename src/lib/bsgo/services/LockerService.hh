74010c388df969c3c5aca3c2eca2956dbe35f15d
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-20 10:36:38 +0100

    feat: LockerService is able to register a weapon in the database when possible

--- src/lib/bsgo/services/LockerService.hh
+++ src/lib/bsgo/services/LockerService.hh
@@ -13,12 +13,21 @@ class LockerService : public AbstractService
   ~LockerService() override = default;
 
   void setPlayerDbId(const Uuid &player);
+  void setPlayerShipDbId(const Uuid &ship);
   bool isReady() const noexcept override;
+
+  bool tryEquip(const Uuid &id, const Item &type) const;
 
   private:
   std::optional<Uuid> m_playerDbId{};
+  std::optional<Uuid> m_playerShipDbId{};
 
   void checkPlayerDbIdExists() const;
+  void checkPlayerShipDbIdExists() const;
+
+  bool verifySlotAvailability(const Item &type) const;
+  auto determineNextSlot(const Item &type) const -> Uuid;
+  void tryEquipWeapon(const Uuid &id, const Uuid slot) const;
 };
 
 using LockerServiceShPtr = std::shared_ptr<LockerService>;
