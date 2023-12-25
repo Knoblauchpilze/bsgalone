08100b528acac8759e96abc984c570850697b090
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-25 10:53:11 +0100

    feat: Added component type

---
+++ src/lib/bsgo/components/ComponentType.cc
@@ -0,0 +1,59 @@
+
+#include "ComponentType.hh"
+
+namespace bsgo {
+
+auto str(const ComponentType &type) -> std::string
+{
+  switch (type)
+  {
+    case ComponentType::AI:
+      return "ai";
+    case ComponentType::COMPUTER_SLOT:
+      return "computer_slot";
+    case ComponentType::DAMAGE:
+      return "damage";
+    case ComponentType::FACTION:
+      return "faction";
+    case ComponentType::HEALTH:
+      return "health";
+    case ComponentType::KIND:
+      return "kind";
+    case ComponentType::LOOT:
+      return "loot";
+    case ComponentType::NAME:
+      return "name";
+    case ComponentType::NETWORK:
+      return "network";
+    case ComponentType::OWNER:
+      return "owner";
+    case ComponentType::POWER:
+      return "power";
+    case ComponentType::REMOVAL:
+      return "removal";
+    case ComponentType::RESOURCE:
+      return "resource";
+    case ComponentType::SCANNED:
+      return "scanned";
+    case ComponentType::SHIP_CLASS:
+      return "ship_class";
+    case ComponentType::SLOT:
+      return "slot";
+    case ComponentType::STATUS:
+      return "status";
+    case ComponentType::TARGET:
+      return "target";
+    case ComponentType::TRANSFORM:
+      return "transform";
+    case ComponentType::VELOCITY:
+      return "velocity";
+    case ComponentType::WEAPON_EFFECT:
+      return "weapon_effect";
+    case ComponentType::WEAPON_SLOT:
+      return "weapon_slot";
+    default:
+      return "unknown";
+  }
+}
+
+} // namespace bsgo
---
+++ src/lib/bsgo/components/ComponentType.hh
@@ -0,0 +1,36 @@
+
+#pragma once
+
+#include <string>
+
+namespace bsgo {
+
+enum class ComponentType
+{
+  AI,
+  COMPUTER_SLOT,
+  DAMAGE,
+  FACTION,
+  HEALTH,
+  KIND,
+  LOOT,
+  NAME,
+  NETWORK,
+  OWNER,
+  POWER,
+  REMOVAL,
+  RESOURCE,
+  SCANNED,
+  SHIP_CLASS,
+  SLOT,
+  STATUS,
+  TARGET,
+  TRANSFORM,
+  VELOCITY,
+  WEAPON_EFFECT,
+  WEAPON_SLOT
+};
+
+auto str(const ComponentType &type) -> std::string;
+
+} // namespace bsgo
