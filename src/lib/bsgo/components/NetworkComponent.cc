47d31bce3037e5953c97482de9dda023fb23775e
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-25 19:44:17 +0100

    feat: NetworkComponent defines a list of things to sync and whether it's dirty or not

--- src/lib/bsgo/components/NetworkComponent.cc
+++ src/lib/bsgo/components/NetworkComponent.cc
@@ -3,9 +3,25 @@
 
 namespace bsgo {
 
-NetworkComponent::NetworkComponent()
+NetworkComponent::NetworkComponent(const std::unordered_set<ComponentType> &toSync)
   : AbstractComponent(ComponentType::NETWORK)
+  , m_componentsToSync(toSync)
 {}
+
+bool NetworkComponent::needsSync() const
+{
+  return m_needsSync;
+}
+
+void NetworkComponent::markForSync(const bool needsSync)
+{
+  m_needsSync = needsSync;
+}
+
+auto NetworkComponent::componentsToSync() const -> const std::unordered_set<ComponentType> &
+{
+  return m_componentsToSync;
+}
 
 void NetworkComponent::update(const float /*elapsedSeconds*/) {}
 
