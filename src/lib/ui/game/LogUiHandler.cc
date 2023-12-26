562eb12c654757bf061c8ecb109c37ffab01fe14
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-26 22:32:45 +0100

    feat: LogUiHandler implements the IMessageListener class

--- src/lib/ui/game/LogUiHandler.cc
+++ src/lib/ui/game/LogUiHandler.cc
@@ -5,6 +5,7 @@ namespace pge {
 
 LogUiHandler::LogUiHandler(const bsgo::Views & /*views*/)
   : IUiHandler("log")
+  , AbstractMessageListener({bsgo::MessageType::SYSTEM})
 {}
 
 void LogUiHandler::initializeMenus(const int /*width*/, const int /*height*/) {}
@@ -19,5 +20,15 @@ void LogUiHandler::render(SpriteRenderer & /*engine*/) const {}
 void LogUiHandler::updateUi() {}
 
 void LogUiHandler::reset() {}
+
+void LogUiHandler::connectToMessageQueue(bsgo::IMessageQueue &messageQueue)
+{
+  messageQueue.addListener(this);
+}
+
+void LogUiHandler::onMessageReceived(const bsgo::IMessage &message)
+{
+  warn("should handle message with type " + bsgo::str(message.type()));
+}
 
 } // namespace pge
