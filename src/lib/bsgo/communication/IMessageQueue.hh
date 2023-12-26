157efc81e236837aac786018758f53d841a20455
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-26 16:14:14 +0100

    feat: Push message and add listener interface

--- src/lib/bsgo/communication/IMessageQueue.hh
+++ src/lib/bsgo/communication/IMessageQueue.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "IMessage.hh"
+#include "IMessageListener.hh"
 #include <memory>
 
 namespace bsgo {
@@ -11,7 +12,10 @@ class IMessageQueue
   public:
   virtual ~IMessageQueue() = default;
 
-  virtual void pushMessage(IMessagePtr message) = 0;
+  virtual void pushMessage(IMessagePtr message)                                        = 0;
+  virtual void addListener(const MessageType &messageType, IMessageListener *listener) = 0;
+
+  virtual void processMessages() = 0;
 };
 
 } // namespace bsgo
--- src/lib/bsgo/communication/MessageQueue.cc
+++ src/lib/bsgo/communication/MessageQueue.cc
@@ -15,4 +15,19 @@ void MessageQueue::pushMessage(IMessagePtr message)
   m_messages.emplace_back(std::move(message));
 }
 
+void MessageQueue::addListener(const MessageType &messageType, IMessageListener *listener)
+{
+  if (nullptr == listener)
+  {
+    error("Failed to add listener", "Null listener");
+  }
+
+  m_listeners.emplace(messageType, listener);
+}
+
+void MessageQueue::processMessages()
+{
+  warn("should process messages");
+}
+
 } // namespace bsgo
