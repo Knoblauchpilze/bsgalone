d2eb3b7968aba9cf59d36bcc505f5cad8f6230a0
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-27 12:47:31 +0100

    feat: Added LootMessage

---
+++ src/lib/bsgo/messages/LootMessage.cc
@@ -0,0 +1,22 @@
+
+#include "LootMessage.hh"
+
+namespace bsgo {
+
+LootMessage::LootMessage(const Uuid &resourceId, const float amount)
+  : SystemMessage(SystemType::LOOT)
+  , m_resourceId(resourceId)
+  , m_amount(amount)
+{}
+
+auto LootMessage::resourceId() const -> Uuid
+{
+  return m_resourceId;
+}
+
+auto LootMessage::amount() const -> float
+{
+  return m_amount;
+}
+
+} // namespace bsgo
