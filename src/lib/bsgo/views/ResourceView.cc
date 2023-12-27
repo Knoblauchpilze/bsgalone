c554eedb008ebcc14cf306784618059e225eb949
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-27 12:25:57 +0100

    feat: Added ResourceView

---
+++ src/lib/bsgo/views/ResourceView.cc
@@ -0,0 +1,15 @@
+
+#include "ResourceView.hh"
+
+namespace bsgo {
+
+ResourceView::ResourceView(const CoordinatorShPtr &coordinator, const Repositories &repositories)
+  : IView("resource", coordinator, repositories)
+{}
+
+auto ResourceView::getResourceName(const Uuid &resource) const -> std::string
+{
+  return m_repositories.resourceRepository->findOneById(resource).name;
+}
+
+} // namespace bsgo
