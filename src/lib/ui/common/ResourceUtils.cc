6491466599ce77f83e69db7a9a52f85fc4a598f8
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-27 12:26:18 +0100

    feat: Added a method to get a color from a resource name

---
+++ src/lib/ui/common/ResourceUtils.cc
@@ -0,0 +1,22 @@
+
+#include "ResourceUtils.hh"
+
+namespace pge {
+constexpr auto TYLIUM_NAME = "tylium";
+constexpr auto TITANE_NAME = "titane";
+
+auto colorFromResourceName(const std::string &name) -> olc::Pixel
+{
+  if (TYLIUM_NAME == name)
+  {
+    return olc::YELLOW;
+  }
+  if (TITANE_NAME == name)
+  {
+    return olc::PURPLE;
+  }
+
+  throw std::invalid_argument("Unsupported resource name " + name);
+}
+
+} // namespace pge
---
+++ src/lib/ui/common/ResourceUtils.hh
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include "olcEngine.hh"
+#include <string>
+
+namespace pge {
+
+auto colorFromResourceName(const std::string &name) -> olc::Pixel;
+
+}
