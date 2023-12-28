49d865aee16647c87bdbcc1b76e8560a3b3e601e
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-28 10:28:29 +0100

    feat: Added ship prices in the database and added the Jormung

--- database/migrations/101_seed_ship.up.sql
+++ database/migrations/101_seed_ship.up.sql
@@ -16,6 +16,41 @@ INSERT INTO public.ship ("name", "faction", "class", "starting_ship", "max_hull_
   VALUES (
       'Jotunn', 'colonial', 'line', false, 3000.0, 50.0, 500.0, 5.0, 1.0, 2.0, 2.0
   );
+INSERT INTO public.ship ("name", "faction", "class", "starting_ship", "max_hull_points", "hull_points_regen", "max_power_points", "power_points_regen", "max_acceleration", "max_speed", "radius")
+  VALUES (
+      'Jormung', 'cylon', 'line', false, 3000.0, 50.0, 500.0, 5.0, 1.0, 2.0, 2.0
+  );
+
+INSERT INTO public.ship_price ("ship", "resource", "cost")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Viper Mark II'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    10000.0
+  );
+INSERT INTO public.ship_price ("ship", "resource", "cost")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Cylon Raider'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    10000.0
+  );
+INSERT INTO public.ship_price ("ship", "resource", "cost")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jotunn'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    200000.0
+  );
+INSERT INTO public.ship_price ("ship", "resource", "cost")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jotunn'),
+    (SELECT id FROM resource WHERE name = 'titane'),
+    100.0
+  );
+INSERT INTO public.ship_price ("ship", "resource", "cost")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    (SELECT id FROM resource WHERE name = 'titane'),
+    100.0
+  );
 
 -- Viper Mark II
 INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
@@ -130,6 +165,57 @@ INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
 INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM ship WHERE name = 'Jotunn'),
+    'computer',
+    0.0, 0.0, 0.0
+  );
+
+-- Jormung
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    -0.5, -0.5, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    -0.5, 0.0, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    -0.5, 0.5, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    0.5, 0.5, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    0.5, 0.0, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'weapon',
+    0.5, -0.5, 0.0
+  );
+
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
+    'computer',
+    0.0, 0.0, 0.0
+  );
+INSERT INTO public.ship_slot ("ship", "type", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM ship WHERE name = 'Jormung'),
     'computer',
     0.0, 0.0, 0.0
   );
