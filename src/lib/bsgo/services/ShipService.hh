70920aa70f4379216748367a027587d785abf24e
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-29 11:09:13 +0100

    feat: ShipService can switch active ships

--- src/lib/bsgo/services/ShipService.hh
+++ src/lib/bsgo/services/ShipService.hh
@@ -13,7 +13,18 @@ class ShipService : public AbstractService
   ShipService(const Repositories &repositories);
   ~ShipService() override = default;
 
+  void setPlayerDbId(const Uuid &player);
+  bool isReady() const noexcept override;
+
   bool trySelectShip(const Uuid &playerShipId) const;
+
+  private:
+  std::optional<Uuid> m_playerDbId{};
+
+  void checkPlayerDbIdExists() const;
+  bool verifyPreconditions(const PlayerShip &ship) const;
+  void switchActiveShip(PlayerShip currentActiveShip, PlayerShip newActiveShip) const;
+  void switchShipSystem(const PlayerShip &currentActiveShip, const PlayerShip &newActiveShip) const;
 };
 
 using ShipServiceShPtr = std::shared_ptr<ShipService>;
