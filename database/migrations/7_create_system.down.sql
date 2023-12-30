817015c7f567662bf2c5c12f94978ea709aaec8d
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-30 19:47:50 +0100

    feat: Added table registering jumping ships

--- database/migrations/7_create_system.down.sql
+++ database/migrations/7_create_system.down.sql
@@ -3,6 +3,7 @@ DROP TABLE system_outpost;
 DROP TABLE outpost;
 DROP TABLE asteroid_loot;
 DROP TABLE asteroid;
+DROP TABLE ship_jump;
 DROP TABLE ship_system;
 DROP TABLE starting_system;
 DROP TABLE system;
