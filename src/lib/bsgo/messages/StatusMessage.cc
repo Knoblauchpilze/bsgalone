488a3ff32a25d59b0d3c797dfd72742c62fcf103
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-30 19:30:20 +0100

    feat: Added the system to which we jump to in the StatusMessage and provided it when using the ShipView

--- src/lib/bsgo/messages/StatusMessage.cc
+++ src/lib/bsgo/messages/StatusMessage.cc
@@ -4,9 +4,16 @@
 namespace bsgo {
 
 StatusMessage::StatusMessage(const Uuid &shipDbId, const JumpState &jumpState)
+  : StatusMessage(shipDbId, jumpState, {})
+{}
+
+StatusMessage::StatusMessage(const Uuid &shipDbId,
+                             const JumpState &jumpState,
+                             const std::optional<Uuid> &system)
   : SystemMessage(SystemType::STATUS)
   , m_shipDbId(shipDbId)
   , m_jumpState(jumpState)
+  , m_system(system)
 {}
 
 auto StatusMessage::getShipDbId() const -> Uuid
@@ -17,6 +24,11 @@ auto StatusMessage::getShipDbId() const -> Uuid
 auto StatusMessage::getJumpState() const -> JumpState
 {
   return m_jumpState;
+}
+
+auto StatusMessage::getJumpSystem() const -> std::optional<Uuid>
+{
+  return m_system;
 }
 
 } // namespace bsgo
--- src/lib/bsgo/messages/StatusMessage.hh
+++ src/lib/bsgo/messages/StatusMessage.hh
@@ -3,6 +3,7 @@
 
 #include "SystemMessage.hh"
 #include "Uuid.hh"
+#include <optional>
 
 namespace bsgo {
 
@@ -17,14 +18,17 @@ class StatusMessage : public SystemMessage
 {
   public:
   StatusMessage(const Uuid &shipDbId, const JumpState &jumpState);
+  StatusMessage(const Uuid &shipDbId, const JumpState &jumpState, const std::optional<Uuid> &system);
   ~StatusMessage() override = default;
 
   auto getShipDbId() const -> Uuid;
   auto getJumpState() const -> JumpState;
+  auto getJumpSystem() const -> std::optional<Uuid>;
 
   private:
   Uuid m_shipDbId{};
   JumpState m_jumpState{};
+  std::optional<Uuid> m_system{};
 };
 
 } // namespace bsgo
