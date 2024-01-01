eb3630469283f190557644d3a1372cbdf91eaf3f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-01-01 11:02:17 +0100

    fix: Unnecessary inline

---
+++ src/lib/coordinates/CenteredViewport.cc
@@ -0,0 +1,87 @@
+
+#include "CenteredViewport.hh"
+
+namespace pge {
+
+CenteredViewport::CenteredViewport(const Vectorf &center, const Vectorf &dims) noexcept
+  : Viewport()
+  , m_center(center)
+  , m_dims(dims)
+{}
+
+auto CenteredViewport::center() const noexcept -> Vectorf
+{
+  return m_center;
+}
+
+auto CenteredViewport::topLeft() const noexcept -> Vectorf
+{
+  return Vectorf(m_center.x - m_dims.x / 2.0f, m_center.y + m_dims.y / 2.0f);
+}
+
+auto CenteredViewport::dims() const noexcept -> Vectorf
+{
+  return m_dims;
+}
+
+auto CenteredViewport::relativeCoords(const float x, const float y) const noexcept -> Vectorf
+{
+  Vectorf out(x, y);
+
+  out.x -= m_center.x;
+  out.y -= m_center.y;
+
+  out.x /= (m_dims.x / 2.0f);
+  out.y /= (m_dims.y / 2.0f);
+
+  return out;
+}
+
+auto CenteredViewport::absoluteCoords(const float x, const float y) const noexcept -> Vectorf
+{
+  Vectorf out(x, y);
+
+  out.x *= (m_dims.x / 2.0f);
+  out.y *= (m_dims.y / 2.0f);
+
+  out.x += m_center.x;
+  out.y += m_center.y;
+
+  return out;
+}
+
+void CenteredViewport::moveTo(const Vectorf &center) noexcept
+{
+  m_center = center;
+}
+
+void CenteredViewport::translate(const Vectorf &delta) noexcept
+{
+  m_center += delta;
+}
+
+void CenteredViewport::scale(const float sx, const float sy) noexcept
+{
+  m_dims.x *= sx;
+  m_dims.y *= sy;
+}
+
+bool CenteredViewport::visible(const float x,
+                               const float y,
+                               const float sx,
+                               const float sy) const noexcept
+{
+  if (x + sx < m_center.x - m_dims.x / 2.0f || x - sx > m_center.x + m_dims.x / 2.0f)
+  {
+    return false;
+  }
+
+  if (y + sy < m_center.y - m_dims.y / 2.0f || y - sy > m_center.y + m_dims.y / 2.0f)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+} // namespace pge
