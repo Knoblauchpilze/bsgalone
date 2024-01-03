82168888f20deb319113bc82a9ade30d0e384cd2
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-03 18:08:25 +0100

    fix: AbstractInputHandler verifies that the queue is not null

--- src/lib/inputs/AbstractInputHandler.cc
+++ src/lib/inputs/AbstractInputHandler.cc
@@ -7,6 +7,11 @@ AbstractInputHandler::AbstractInputHandler(const std::string &name,
                                            bsgo::IMessageQueue *const messageQueue)
   : IInputHandler(name)
   , m_messageQueue(messageQueue)
-{}
+{
+  if (nullptr == messageQueue)
+  {
+    throw std::invalid_argument("Expected non null message queue");
+  }
+}
 
 } // namespace pge
