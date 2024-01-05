3557a62533b763be0ec58c6105feb58c3101a341
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-05 15:56:20 +0100

    feat: Added consumer for equip messages and registered it in the Game

---
+++ src/lib/bsgo/communication/consumers/EquipMessageConsumer.cc
@@ -0,0 +1,88 @@
+
+#include "EquipMessageConsumer.hh"
+#include "EquipMessage.hh"
+
+namespace bsgo {
+
+EquipMessageConsumer::EquipMessageConsumer(const Services &services,
+                                           IMessageQueue *const messageQueue)
+  : AbstractMessageConsumer("equip", {MessageType::EQUIP})
+  , m_lockerService(services.locker)
+  , m_messageQueue(messageQueue)
+{
+  if (nullptr == m_lockerService)
+  {
+    throw std::invalid_argument("Expected non null locker service");
+  }
+  if (nullptr == m_messageQueue)
+  {
+    throw std::invalid_argument("Expected non null message queue");
+  }
+}
+
+void EquipMessageConsumer::onMessageReceived(const IMessage &message)
+{
+  const auto &equip       = message.as<EquipMessage>();
+  const auto equipRequest = EquipState::REQUESTED == equip.getEquipState();
+  if (!equipRequest)
+  {
+    return;
+  }
+
+  const auto shipDbId = equip.getShipDbId();
+  const auto itemType = equip.getItemType();
+  const auto itemDbId = equip.getItemDbId();
+
+  const auto action = equip.getAction();
+  switch (action)
+  {
+    case EquipType::EQUIP:
+      handleEquipRequest(shipDbId, itemType, itemDbId);
+      break;
+    case EquipType::UNEQUIP:
+      handleUnequipRequest(shipDbId, itemType, itemDbId);
+      break;
+    default:
+      error("Unsupported action type for equip message");
+  }
+}
+
+void EquipMessageConsumer::handleEquipRequest(const Uuid &shipDbId,
+                                              const Item &type,
+                                              const Uuid &itemDbId) const
+{
+  const bsgo::LockerItemData data{.dbId = itemDbId, .type = type, .shipDbId = shipDbId};
+  if (!m_lockerService->tryEquip(data))
+  {
+    warn("Failed to process equip message for ship " + str(shipDbId) + " for " + str(type) + " "
+         + str(itemDbId));
+    return;
+  }
+
+  m_messageQueue->pushMessage(std::make_unique<EquipMessage>(EquipType::EQUIP,
+                                                             shipDbId,
+                                                             type,
+                                                             itemDbId,
+                                                             EquipState::COMPLETED));
+}
+
+void EquipMessageConsumer::handleUnequipRequest(const Uuid &shipDbId,
+                                                const Item &type,
+                                                const Uuid &itemDbId) const
+{
+  const bsgo::LockerItemData data{.dbId = itemDbId, .type = type, .shipDbId = shipDbId};
+  if (!m_lockerService->tryUnequip(data))
+  {
+    warn("Failed to process unequip message for ship " + str(shipDbId) + " for " + str(type) + " "
+         + str(itemDbId));
+    return;
+  }
+
+  m_messageQueue->pushMessage(std::make_unique<EquipMessage>(EquipType::UNEQUIP,
+                                                             shipDbId,
+                                                             type,
+                                                             itemDbId,
+                                                             EquipState::COMPLETED));
+}
+
+} // namespace bsgo
