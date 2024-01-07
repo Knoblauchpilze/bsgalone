8a2740b5b301b64c6c5bc795f2c4331429b3cee5
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-01-07 23:12:28 +0100

    feat: Struct color to mask the olc::Pixel one

--- src/pge/colors/Transparency.hh
+++ src/pge/colors/Transparency.hh
@@ -1,17 +1,10 @@
 
 #pragma once
 
+#include "Alpha.hh"
 #include "olcEngine.hh"
 
 namespace pge {
-namespace alpha {
-/// @brief  @brief - Alpha values for various transparency levels.
-constexpr uint8_t OPAQUE             = 255;
-constexpr uint8_t ALMOST_OPAQUE      = 192;
-constexpr uint8_t SEMI_OPAQUE        = 128;
-constexpr uint8_t ALMOST_TRANSPARENT = 64;
-constexpr uint8_t TRANSPARENT        = 0;
-} // namespace alpha
 
 auto opacityFromPercentage(const float perc) -> uint8_t;
 
