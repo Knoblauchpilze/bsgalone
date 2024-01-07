43c6382d3ee72773b57b3c10c8f13f470b141cf3
KnoblauchPilze <prolific-programmer@tuta.com>
2024-01-07 23:12:28 +0100

    feat: Struct color to mask the olc::Pixel one

---
+++ src/pge/colors/Color.hh
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include "Alpha.hh"
+#include "Colors.hh"
+#include "RgbData.hh"
+#include <optional>
+
+namespace pge {
+
+struct Color
+{
+  std::optional<colors::Name> name{};
+  std::optional<RgbData> rgb{};
+  uint8_t a{alpha::OPAQUE};
+};
+
+} // namespace pge
