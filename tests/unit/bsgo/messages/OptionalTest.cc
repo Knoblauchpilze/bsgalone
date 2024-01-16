d09f4e8d0ea260cb4fa44db9ba086fb71459008c
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-16 23:14:17 +0100

    feat: Added more tests for std::optional and Uuid

---
+++ tests/unit/bsgo/messages/OptionalTest.cc
@@ -0,0 +1,82 @@
+
+#include "Common.hh"
+#include "Faction.hh"
+#include "Slot.hh"
+#include "Uuid.hh"
+#include <gtest/gtest.h>
+#include <optional>
+
+using namespace ::testing;
+
+namespace bsgo {
+TEST(Unit_Bsgo_Serialization_Optional_Uuid, Empty)
+{
+  const std::optional<Uuid> expected{};
+  std::optional<Uuid> actual{Uuid{1}};
+  serializeAndDeserialize(expected, actual);
+  EXPECT_EQ(actual, expected);
+}
+
+TEST(Unit_Bsgo_Serialization_Optional_Uuid, WithValue)
+{
+  const std::optional<Uuid> expected{Uuid{14}};
+  std::optional<Uuid> actual{};
+  serializeAndDeserialize(expected, actual);
+  EXPECT_EQ(actual, expected);
+}
+
+TEST(Unit_Bsgo_Serialization_Optional_Uuid, EmptyAndSomeValueAfter)
+{
+  const std::optional<Uuid> expectedOpt{};
+  const auto expectedFloat{1.68f};
+
+  std::ostringstream out;
+  utils::serialize(out, expectedOpt);
+  utils::serialize(out, expectedFloat);
+  std::istringstream in(out.str());
+
+  std::optional<Uuid> actualOpt{};
+  float actualFloat{};
+  utils::deserialize(in, actualOpt);
+  utils::deserialize(in, actualFloat);
+
+  EXPECT_EQ(actualOpt, expectedOpt);
+  EXPECT_EQ(actualFloat, expectedFloat);
+}
+
+TEST(Unit_Bsgo_Serialization_Optional_Uuid, WithValueAndSomeValueAfter)
+{
+  const std::optional<Uuid> expectedOpt{};
+  const auto expectedSlot{Slot::COMPUTER};
+
+  std::ostringstream out;
+  utils::serialize(out, expectedOpt);
+  utils::serialize(out, expectedSlot);
+  std::istringstream in(out.str());
+
+  std::optional<Uuid> actualOpt{};
+  Slot actualSlot{};
+  utils::deserialize(in, actualOpt);
+  utils::deserialize(in, actualSlot);
+
+  EXPECT_EQ(actualOpt, expectedOpt);
+  EXPECT_EQ(actualSlot, expectedSlot);
+}
+
+TEST(Unit_Bsgo_Serialization_Optional_Enum, Empty)
+{
+  const std::optional<Faction> expected{};
+  std::optional<Faction> actual{Faction::COLONIAL};
+  serializeAndDeserialize(expected, actual);
+  EXPECT_EQ(actual, expected);
+}
+
+TEST(Unit_Bsgo_Serialization_Optional_Enum, WithValue)
+{
+  const std::optional<Faction> expected{Faction::CYLON};
+  std::optional<Faction> actual{};
+  serializeAndDeserialize(expected, actual);
+  EXPECT_EQ(actual, expected);
+}
+
+} // namespace bsgo
