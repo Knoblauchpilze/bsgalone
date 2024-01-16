ab5fe83c01c45b41aee267e435365a6fed81256a
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-16 22:41:34 +0100

    feat: Added unit tests for de/serialization of string, MessageType and DockMessage

---
+++ tests/unit/bsgo/messages/Common.hh
@@ -0,0 +1,11 @@
+
+#pragma once
+
+namespace bsgo {
+
+template<typename T>
+auto serializeAndDeserialize(const T &value) -> T;
+
+}
+
+#include "Common.hxx"
---
+++ tests/unit/bsgo/messages/MessageTypeTest.cc
@@ -0,0 +1,49 @@
+
+#include "MessageType.hh"
+#include <core_utils/SerializationUtils.hh>
+#include <gtest/gtest.h>
+#include <sstream>
+
+using namespace ::testing;
+
+namespace bsgo {
+
+struct TestCaseMessageType
+{
+  MessageType type{};
+};
+
+using TestCaseSerialization = TestWithParam<TestCaseMessageType>;
+
+TEST_P(TestCaseSerialization, Serialization)
+{
+  const auto &param = GetParam();
+
+  std::ostringstream out;
+  utils::serialize(out, param.type);
+  std::istringstream in(out.str());
+
+  MessageType expected;
+  utils::deserialize(in, expected);
+
+  EXPECT_EQ(param.type, expected);
+}
+
+INSTANTIATE_TEST_CASE_P(Unit_Bsgo_Serialization_MessageType,
+                        TestCaseSerialization,
+                        Values(TestCaseMessageType{MessageType::DOCK},
+                               TestCaseMessageType{MessageType::EQUIP},
+                               TestCaseMessageType{MessageType::HANGAR},
+                               TestCaseMessageType{MessageType::JUMP},
+                               TestCaseMessageType{MessageType::LOGIN},
+                               TestCaseMessageType{MessageType::LOOT},
+                               TestCaseMessageType{MessageType::PURCHASE},
+                               TestCaseMessageType{MessageType::SCANNED},
+                               TestCaseMessageType{MessageType::SLOT},
+                               TestCaseMessageType{MessageType::VELOCITY},
+                               TestCaseMessageType{MessageType::TARGET}),
+                        [](const TestParamInfo<TestCaseMessageType> &info) -> std::string {
+                          return str(info.param.type);
+                        });
+
+} // namespace bsgo
