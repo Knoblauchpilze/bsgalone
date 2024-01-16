ef98a266ff8ad6bd20890948e8d311935ce176ec
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-16 23:14:17 +0100

    feat: Added more tests for std::optional and Uuid

---
+++ tests/unit/bsgo/messages/UuidTest.cc
@@ -0,0 +1,37 @@
+
+#include "Uuid.hh"
+#include "Common.hh"
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+namespace bsgo {
+
+TEST(Unit_Bsgo_Serialization_Uuid, SingleValue)
+{
+  const Uuid expected{2};
+  Uuid actual{1};
+  serializeAndDeserialize(expected, actual);
+  EXPECT_EQ(actual, expected);
+}
+
+TEST(Unit_Bsgo_Serialization_Uuid, MultipleValues)
+{
+  Uuid expected1{2};
+  Uuid expected2{36};
+
+  std::ostringstream out;
+  utils::serialize(out, expected1);
+  utils::serialize(out, expected2);
+  std::istringstream in(out.str());
+
+  Uuid actual1{3};
+  Uuid actual2{27};
+  utils::deserialize(in, actual1);
+  utils::deserialize(in, actual2);
+
+  EXPECT_EQ(actual1, expected1);
+  EXPECT_EQ(actual2, expected2);
+}
+
+} // namespace bsgo
