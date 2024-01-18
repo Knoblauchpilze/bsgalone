f8aa827f19e990f5578bafb7dea86e7c4df83c7b
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-18 23:23:38 +0100

    feat: Vector deserialization also returns a boolean for success

--- src/bsgo/common/VectorUtils.hh
+++ src/bsgo/common/VectorUtils.hh
@@ -9,6 +9,6 @@ namespace bsgo {
 auto str(const Eigen::Vector3f &v) -> std::string;
 
 auto serialize(std::ostream &out, const Eigen::Vector3f &v) -> std::ostream &;
-auto deserialize(std::istream &in, Eigen::Vector3f &v) -> std::istream &;
+bool deserialize(std::istream &in, Eigen::Vector3f &v);
 
 } // namespace bsgo
