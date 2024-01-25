d878b1154e1fa84c50cf63ae75e236c87369f0c4
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-25 17:53:48 +0100

    feat: Server uses a BroadcastMessageQueue to send messages to the right client

---
+++ src/server/lib/network/BroadcastMessageQueue.cc
@@ -0,0 +1,70 @@
+
+#include "BroadcastMessageQueue.hh"
+#include "MessageProcessor.hh"
+#include "NetworkMessage.hh"
+
+namespace bsgo {
+
+BroadcastMessageQueue::BroadcastMessageQueue()
+  : utils::CoreObject("broadcast")
+{
+  addModule("queue");
+  setService("message");
+}
+
+void BroadcastMessageQueue::registerClient(const Uuid clientId, net::ConnectionShPtr connection)
+{
+  const std::lock_guard guard(m_locker);
+
+  const auto [_, inserted] = m_clients.try_emplace(clientId, connection);
+  if (!inserted)
+  {
+    error("Failed to register client " + str(clientId), "Such a client already exists");
+  }
+}
+
+void BroadcastMessageQueue::pushMessage(IMessagePtr message)
+{
+  const std::lock_guard guard(m_locker);
+  m_messages.emplace_back(std::move(message));
+}
+
+void BroadcastMessageQueue::addListener(IMessageListener * /*listener*/)
+{
+  error("Failed to add listener", "Broadcast message queue does not support listeners");
+}
+
+bool BroadcastMessageQueue::empty()
+{
+  const std::lock_guard guard(m_locker);
+  return m_messages.empty();
+}
+
+void BroadcastMessageQueue::processMessages(const std::optional<int> &amount)
+{
+  MessageProcessor processor(m_messages, &m_locker, [this](const IMessage &message) {
+    processMessage(message);
+  });
+
+  processor.processMessages(amount);
+}
+
+void BroadcastMessageQueue::processMessage(const IMessage &message)
+{
+  if (!message.isA<NetworkMessage>())
+  {
+    error("Unsupported message type " + str(message.type()), "Message is not a network message");
+  }
+
+  const auto &network = message.as<NetworkMessage>();
+  auto maybeClient    = m_clients.find(network.getClientId());
+  if (maybeClient == m_clients.end())
+  {
+    error("Failed to send message " + str(message.type()),
+          "Unknown client " + str(network.getClientId()));
+  }
+
+  maybeClient->second->send(message);
+}
+
+} // namespace bsgo
---
+++ src/server/lib/network/BroadcastMessageQueue.hh
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "Connection.hh"
+#include "IMessageQueue.hh"
+#include "Uuid.hh"
+#include <core_utils/CoreObject.hh>
+#include <deque>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+namespace bsgo {
+
+class BroadcastMessageQueue : public IMessageQueue, public utils::CoreObject
+{
+  public:
+  BroadcastMessageQueue();
+  ~BroadcastMessageQueue() override = default;
+
+  void registerClient(const Uuid clientId, net::ConnectionShPtr connection);
+
+  void pushMessage(IMessagePtr message) override;
+  void addListener(IMessageListener *listener) override;
+  bool empty() override;
+
+  void processMessages(const std::optional<int> &amount = {}) override;
+
+  private:
+  std::mutex m_locker{};
+  std::deque<IMessagePtr> m_messages{};
+  std::unordered_map<Uuid, net::ConnectionShPtr> m_clients{};
+
+  void processMessage(const IMessage &message);
+};
+
+using BroadcastMessageQueuePtr = std::unique_ptr<BroadcastMessageQueue>;
+
+} // namespace bsgo
