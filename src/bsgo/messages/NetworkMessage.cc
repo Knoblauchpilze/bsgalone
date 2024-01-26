3586caa507b2148079693f970f05516916bdeb9b
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-26 17:30:23 +0100

    feat: Added a method in the NetworkMessage to copy a client id

--- src/bsgo/messages/NetworkMessage.cc
+++ src/bsgo/messages/NetworkMessage.cc
@@ -26,4 +26,13 @@ auto NetworkMessage::tryGetClientId() const -> std::optional<Uuid>
   return m_clientId;
 }
 
+void NetworkMessage::copyClientIdIfDefined(const NetworkMessage &source)
+{
+  const auto maybeClientId = source.tryGetClientId();
+  if (maybeClientId)
+  {
+    m_clientId = maybeClientId;
+  }
+}
+
 } // namespace bsgo
