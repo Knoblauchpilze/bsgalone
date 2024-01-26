2b6352e2cdcf46b2eda8eb2046927899f8a39371
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-26 17:00:19 +0100

    feat: Made the DataSource register all players in a system and robust to not provide any player

--- src/bsgo/data/PlayerDataSource.hh
+++ src/bsgo/data/PlayerDataSource.hh
@@ -12,14 +12,20 @@ class Coordinator;
 class PlayerDataSource : public utils::CoreObject
 {
   public:
-  PlayerDataSource(const Repositories &repositories, const Uuid &playerDbId);
+  PlayerDataSource(const Repositories &repositories,
+                   const Uuid systemDbId,
+                   const std::optional<Uuid> &playerDbId);
   ~PlayerDataSource() override = default;
 
-  auto initialize(Coordinator &coordinator) const -> Uuid;
+  auto initialize(Coordinator &coordinator) const -> std::optional<Uuid>;
 
   private:
-  Uuid m_playerDbId{};
+  Uuid m_systemDbId{};
+  std::optional<Uuid> m_playerDbId{};
+  mutable std::optional<Uuid> m_playerEntityId{};
   Repositories m_repositories{};
+
+  void registerPlayer(Coordinator &coordinator, const Uuid playerId) const;
 };
 
 } // namespace bsgo
