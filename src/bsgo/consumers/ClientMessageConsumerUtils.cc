929e2c52973992ba6414613d1cbf5f87d7516c65
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-28 11:20:14 +0100

    feat: Moved SlotMessageConsumer to the server

--- src/bsgo/consumers/ClientMessageConsumerUtils.cc
+++ src/bsgo/consumers/ClientMessageConsumerUtils.cc
@@ -1,7 +1,6 @@
 
 #include "ClientMessageConsumerUtils.hh"
 
-#include "SlotMessageConsumer.hh"
 #include "TargetMessageConsumer.hh"
 #include "VelocityMessageConsumer.hh"
 
@@ -11,9 +10,6 @@ void createMessageConsumers(IMessageQueue &inputMessagesQueue,
                             IMessageQueue *const outputMessagesQueue,
                             const Services &services)
 {
-  auto slot = std::make_unique<SlotMessageConsumer>(services, outputMessagesQueue);
-  inputMessagesQueue.addListener(std::move(slot));
-
   auto velocity = std::make_unique<VelocityMessageConsumer>(services, outputMessagesQueue);
   inputMessagesQueue.addListener(std::move(velocity));
 
