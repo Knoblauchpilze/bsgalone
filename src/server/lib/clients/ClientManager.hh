1d549987fb5939890bdb9396a1935cdee5b368d5
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-01-29 20:10:01 +0100

    feat: ClientManager expects the system to be provided when registering a player

--- src/server/lib/clients/ClientManager.hh
+++ src/server/lib/clients/ClientManager.hh
@@ -18,12 +18,13 @@ class ClientManager : public utils::CoreObject
   ~ClientManager() override = default;
 
   auto registerConnection(const net::ConnectionShPtr connection) -> Uuid;
-  void registerPlayer(const Uuid clientId, const Uuid playerDbId);
+  void registerPlayer(const Uuid clientId, const Uuid playerDbId, const Uuid playerSystemDbId);
   void removePlayer(const Uuid playerDbId);
   void removeConnection(const net::ConnectionId connectionId);
 
   auto getConnectionForClient(const Uuid clientId) const -> net::ConnectionShPtr;
   auto getAllConnections() const -> std::vector<net::ConnectionShPtr>;
+  auto tryGetSystemForClient(const Uuid clientId) const -> std::optional<Uuid>;
 
   private:
   static Uuid NEXT_CLIENT_ID;
@@ -32,6 +33,7 @@ class ClientManager : public utils::CoreObject
   {
     Uuid clientId{};
     std::optional<Uuid> playerDbId{};
+    std::optional<Uuid> playerSystemDbId{};
     net::ConnectionShPtr connection{};
   };
 
