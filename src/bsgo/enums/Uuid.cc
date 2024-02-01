7afe6309b637c5b7f884e9f45cb3b4c75e78a38b
KnoblauchPilze <prolific-programmer@tuta.com>
2024-02-01 22:08:56 +0100

    fix: Sanitization of Uuid&

--- src/bsgo/enums/Uuid.cc
+++ src/bsgo/enums/Uuid.cc
@@ -3,17 +3,17 @@
 
 namespace bsgo {
 
-auto str(const Uuid &id) -> std::string
+auto str(const Uuid id) -> std::string
 {
   return std::to_string(id);
 }
 
-auto toDbId(const Uuid &id) -> int
+auto toDbId(const Uuid id) -> int
 {
   return static_cast<int>(id) + 1u;
 }
 
-auto fromDbId(const int &dbId) -> Uuid
+auto fromDbId(const int dbId) -> Uuid
 {
   return Uuid{dbId - 1};
 }
--- src/bsgo/enums/Uuid.hh
+++ src/bsgo/enums/Uuid.hh
@@ -7,8 +7,8 @@ namespace bsgo {
 
 using Uuid = int;
 
-auto str(const Uuid &id) -> std::string;
-auto toDbId(const Uuid &id) -> int;
-auto fromDbId(const int &dbId) -> Uuid;
+auto str(const Uuid id) -> std::string;
+auto toDbId(const Uuid id) -> int;
+auto fromDbId(const int dbId) -> Uuid;
 
 } // namespace bsgo
