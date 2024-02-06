35bba284c1c28ae31bad1c384fd4135dd6200728
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-06 20:33:47 +0100

    feat: A few asteroids also in 169 aretis

--- database/migrations/120_seed_system.up.sql
+++ database/migrations/120_seed_system.up.sql
@@ -15,6 +15,11 @@ INSERT INTO public.starting_system ("system", "faction")
     'colonial'
   );
 
+INSERT INTO outpost ("faction", "max_hull_points", "hull_points_regen", "max_power_points", "power_points_regen", "radius")
+  VALUES ('colonial', 30000.0, 120.0, 4500.0, 100.0, 2.0);
+INSERT INTO outpost ("faction", "max_hull_points", "hull_points_regen", "max_power_points", "power_points_regen", "radius")
+  VALUES ('cylon', 30000.0, 120.0, 4500.0, 100.0, 2.0);
+
 INSERT INTO public.ship_system ("ship", "system", "docked")
   VALUES (
     (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = 'colo' AND player_ship.active = true),
@@ -40,50 +45,41 @@ INSERT INTO public.ship_system ("ship", "system", "docked")
     false
   );
 
+-- Munnin
 INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM system WHERE name = 'Munnin'),
     85.0,
     0.5,
-    1.0,
-    2.0,
-    0.0
+    1.0, 2.0, 0.0
   );
 INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM system WHERE name = 'Munnin'),
     222.0,
     1.0,
-    4.0,
-    2.0,
-    0.0
+    4.0, 2.0, 0.0
   );
 INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM system WHERE name = 'Munnin'),
     150.0,
     0.5,
-    -2.0,
-    -6.0,
-    0.0
+    -2.0, -6.0, 0.0
   );
 INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM system WHERE name = 'Munnin'),
     3333.0,
     1.0,
-    -3.0,
-    -4.0,
-    0.0
+    -3.0, -4.0, 0.0
   );
 INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM system WHERE name = 'Munnin'),
     2345.0,
     1.0,
-    15.0,
-    17.0,
-    0.0
+    15.0, 17.0, 0.0
   );
 
 INSERT INTO public.asteroid_loot ("asteroid", "resource", "amount")
@@ -99,20 +95,13 @@ INSERT INTO public.asteroid_loot ("asteroid", "resource", "amount")
     2.0
   );
 
-INSERT INTO outpost ("faction", "max_hull_points", "hull_points_regen", "max_power_points", "power_points_regen", "radius")
-  VALUES ('colonial', 30000.0, 120.0, 4500.0, 100.0, 2.0);
-INSERT INTO outpost ("faction", "max_hull_points", "hull_points_regen", "max_power_points", "power_points_regen", "radius")
-  VALUES ('cylon', 30000.0, 120.0, 4500.0, 100.0, 2.0);
-
 INSERT INTO public.system_outpost ("outpost", "system", "hull_points", "power_points", "x_pos", "y_pos", "z_pos")
   VALUES (
     (SELECT id FROM outpost WHERE faction = 'colonial'),
     (SELECT id FROM system WHERE name = 'Munnin'),
     12728.0,
     732.0,
-    -6.0,
-    3.2,
-    0.0
+    -6.0, 3.2, 0.0
   );
 INSERT INTO public.system_outpost ("outpost", "system", "hull_points", "power_points", "x_pos", "y_pos", "z_pos")
   VALUES (
@@ -120,7 +109,37 @@ INSERT INTO public.system_outpost ("outpost", "system", "hull_points", "power_po
     (SELECT id FROM system WHERE name = 'Munnin'),
     13400.0,
     152.0,
-    6.0,
-    -3.2,
-    0.0
+    6.0, -3.2, 0.0
+  );
+
+-- 169 aretis
+INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM system WHERE name = '169 aretis'),
+    40.0,
+    1.0,
+    -1.0, -2.0, 0.0
+  );
+INSERT INTO public.asteroid ("system", "health", "radius", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM system WHERE name = '169 aretis'),
+    120.0,
+    2.0,
+    3.0, 5.0, 0.0
+  );
+
+INSERT INTO public.asteroid_loot ("asteroid", "resource", "amount")
+  VALUES (
+    (SELECT id FROM asteroid WHERE health = 40.0),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    1234.0
+  );
+
+INSERT INTO public.system_outpost ("outpost", "system", "hull_points", "power_points", "x_pos", "y_pos", "z_pos")
+  VALUES (
+    (SELECT id FROM outpost WHERE faction = 'colonial'),
+    (SELECT id FROM system WHERE name = '169 aretis'),
+    5000.0,
+    600.0,
+    -5.0, 0.2, 0.0
   );
