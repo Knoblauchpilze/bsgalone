78aae1f87afb6abbde437564129d3082b3f6cd07
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-06 22:25:24 +0100

    feat: ShipDataSource has a public method to register a ship

--- src/bsgo/data/ShipDataSource.hh
+++ src/bsgo/data/ShipDataSource.hh
@@ -15,20 +15,25 @@ class Coordinator;
 class ShipDataSource : public utils::CoreObject
 {
   public:
-  ShipDataSource(const Repositories &repositories, const Uuid systemDbId);
+  ShipDataSource(const Repositories &repositories);
   ~ShipDataSource() override = default;
 
-  void initialize(Coordinator &coordinator, DatabaseEntityMapper &entityMapper) const;
-
-  private:
-  Uuid m_systemDbId{};
-  std::optional<Uuid> m_playerDbId{};
-  Repositories m_repositories{};
+  void initialize(const Uuid systemDbId,
+                  Coordinator &coordinator,
+                  DatabaseEntityMapper &entityMapper) const;
 
   void registerShip(Coordinator &coordinator,
                     const Uuid ship,
                     DatabaseEntityMapper &entityMapper) const;
 
+  private:
+  std::optional<Uuid> m_playerDbId{};
+  Repositories m_repositories{};
+
+  void registerShip(Coordinator &coordinator,
+                    const Uuid ship,
+                    DatabaseEntityMapper &entityMapper,
+                    const bool ignoreDocked) const;
   void registerShipOwner(Coordinator &coordinator,
                          const Uuid shipEntity,
                          const PlayerShip &shipData,
