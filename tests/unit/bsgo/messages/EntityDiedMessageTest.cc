06a8789815dc23801c254c9ced6cf26f029437d3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-08 22:19:17 +0100

    feat: Added a system to the EntityDiedMessage

--- tests/unit/bsgo/messages/EntityDiedMessageTest.cc
+++ tests/unit/bsgo/messages/EntityDiedMessageTest.cc
@@ -1,6 +1,6 @@
 
-#include "Common.hh"
 #include "EntityDiedMessage.hh"
+#include "Common.hh"
 #include <gtest/gtest.h>
 
 using namespace ::testing;
@@ -12,25 +12,23 @@ auto assertMessagesAreEqual(const EntityDiedMessage &actual, const EntityDiedMes
   EXPECT_EQ(actual.type(), expected.type());
   EXPECT_EQ(actual.getEntityDbId(), expected.getEntityDbId());
   EXPECT_EQ(actual.getEntityKind(), expected.getEntityKind());
+  EXPECT_EQ(actual.tryGetSystemDbId(), expected.tryGetSystemDbId());
   EXPECT_EQ(actual.tryGetClientId(), expected.tryGetClientId());
-  EXPECT_EQ(actual.validated(), expected.validated());
 }
 } // namespace
 
-TEST(Unit_Bsgo_Serialization_EntityDiedMessage, Basic)
+TEST(Unit_Bsgo_Serialization_EntityDiedMessage, EntityAndKind)
 {
   const EntityDiedMessage expected(Uuid{789}, EntityKind::BULLET);
-  EntityDiedMessage actual(Uuid{10}, EntityKind::OUTPOST);
-  actual.validate();
+  EntityDiedMessage actual(Uuid{10}, EntityKind::OUTPOST, Uuid{8564});
   actual.setClientId(Uuid{47});
   serializeAndDeserializeMessage(expected, actual);
   assertMessagesAreEqual(actual, expected);
 }
 
-TEST(Unit_Bsgo_Serialization_EntityDiedMessage, Validated)
+TEST(Unit_Bsgo_Serialization_EntityDiedMessage, EntityKindAndSystem)
 {
-  EntityDiedMessage expected(Uuid{789}, EntityKind::BULLET);
-  expected.validate();
+  EntityDiedMessage expected(Uuid{789}, EntityKind::BULLET, Uuid{10001});
   EntityDiedMessage actual(Uuid{10}, EntityKind::OUTPOST);
   serializeAndDeserializeMessage(expected, actual);
   assertMessagesAreEqual(actual, expected);
@@ -40,14 +38,14 @@ TEST(Unit_Bsgo_Serialization_EntityDiedMessage, WithClientId)
 {
   EntityDiedMessage expected(Uuid{789}, EntityKind::BULLET);
   expected.setClientId(Uuid{36});
-  EntityDiedMessage actual(Uuid{10}, EntityKind::OUTPOST);
+  EntityDiedMessage actual(Uuid{10}, EntityKind::OUTPOST, Uuid{7777});
   serializeAndDeserializeMessage(expected, actual);
   assertMessagesAreEqual(actual, expected);
 }
 
 TEST(Unit_Bsgo_Serialization_EntityDiedMessage, Clone)
 {
-  EntityDiedMessage expected(Uuid{789}, EntityKind::ASTEROID);
+  EntityDiedMessage expected(Uuid{789}, EntityKind::ASTEROID, Uuid{123});
   const auto cloned = expected.clone();
   ASSERT_EQ(cloned->type(), MessageType::ENTITY_DIED);
   assertMessagesAreEqual(cloned->as<EntityDiedMessage>(), expected);
