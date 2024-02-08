d12d56ce07f70951de865b6f111d374240221cd9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-08 21:29:09 +0100

    feat: EntityDiedMessageConsumer takes a combat service as argument

--- src/server/lib/consumers/EntityDiedMessageConsumer.hh
+++ src/server/lib/consumers/EntityDiedMessageConsumer.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "AbstractMessageConsumer.hh"
+#include "CombatService.hh"
 #include "EntityDiedMessage.hh"
 #include "IMessageQueue.hh"
 
@@ -10,12 +11,13 @@ namespace bsgo {
 class EntityDiedMessageConsumer : public AbstractMessageConsumer
 {
   public:
-  EntityDiedMessageConsumer(IMessageQueue *const messageQueue);
+  EntityDiedMessageConsumer(CombatServiceShPtr combatService, IMessageQueue *const messageQueue);
   ~EntityDiedMessageConsumer() override = default;
 
   void onMessageReceived(const IMessage &message) override;
 
   private:
+  CombatServiceShPtr m_combatService{};
   IMessageQueue *const m_messageQueue{};
 
   void handleShipEntityDied(const Uuid shipDbId) const;
