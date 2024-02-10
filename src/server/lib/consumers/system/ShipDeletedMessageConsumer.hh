685fce4ec4065777011ff70ca32e4bf1394f56ca
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-10 15:30:12 +0100

    feat: ShipDeletedMessageConsumer handles the deletion of the ship

--- src/server/lib/consumers/system/ShipDeletedMessageConsumer.hh
+++ src/server/lib/consumers/system/ShipDeletedMessageConsumer.hh
@@ -2,18 +2,20 @@
 #pragma once
 
 #include "AbstractMessageConsumer.hh"
+#include "Services.hh"
 
 namespace bsgo {
 
 class ShipDeletedMessageConsumer : public AbstractMessageConsumer
 {
   public:
-  ShipDeletedMessageConsumer();
+  ShipDeletedMessageConsumer(const Services &services);
   ~ShipDeletedMessageConsumer() override = default;
 
   void onMessageReceived(const IMessage &message) override;
 
   private:
+  ShipServiceShPtr m_shipService{};
 };
 
 } // namespace bsgo
