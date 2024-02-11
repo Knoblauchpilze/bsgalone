e364ecc294fbda94dd5d57f04c56b6ed47d181df
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-11 14:45:36 +0100

    feat: LogoutMessage includes whether or not the connection should be closed

--- src/bsgo/messages/LogoutMessage.cc
+++ src/bsgo/messages/LogoutMessage.cc
@@ -9,13 +9,23 @@ LogoutMessage::LogoutMessage()
 {}
 
 LogoutMessage::LogoutMessage(const Uuid playerDbId)
+  : LogoutMessage(playerDbId, false)
+{}
+
+LogoutMessage::LogoutMessage(const Uuid playerDbId, const bool closeConnection)
   : ValidatableMessage(MessageType::LOGOUT)
   , m_playerDbId(playerDbId)
+  , m_closeConnection(closeConnection)
 {}
 
 auto LogoutMessage::getPlayerDbId() const -> Uuid
 {
   return m_playerDbId;
+}
+
+bool LogoutMessage::shouldCloseConnection() const
+{
+  return m_closeConnection;
 }
 
 auto LogoutMessage::serialize(std::ostream &out) const -> std::ostream &
@@ -25,6 +35,7 @@ auto LogoutMessage::serialize(std::ostream &out) const -> std::ostream &
   utils::serialize(out, m_validated);
 
   utils::serialize(out, m_playerDbId);
+  utils::serialize(out, m_closeConnection);
 
   return out;
 }
@@ -37,13 +48,14 @@ bool LogoutMessage::deserialize(std::istream &in)
   ok &= utils::deserialize(in, m_validated);
 
   ok &= utils::deserialize(in, m_playerDbId);
+  ok &= utils::deserialize(in, m_closeConnection);
 
   return ok;
 }
 
 auto LogoutMessage::clone() const -> IMessagePtr
 {
-  auto clone = std::make_unique<LogoutMessage>(m_playerDbId);
+  auto clone = std::make_unique<LogoutMessage>(m_playerDbId, m_closeConnection);
   clone->copyClientIdIfDefined(*this);
   clone->validate(validated());
 
