34f085a5b0061b5f92880df9e8c6ce8cf59f2970
KnoblauchPilze <prolific-programmer@tuta.com>
2024-02-16 17:46:48 +0100

    feat: Added de/serialization for optional<Vector3f>

--- src/bsgo/common/VectorUtils.cc
+++ src/bsgo/common/VectorUtils.cc
@@ -17,23 +17,49 @@ auto str(const Eigen::Vector3f &v) -> std::string
   return out;
 }
 
-auto serialize(std::ostream &out, const Eigen::Vector3f &v) -> std::ostream &
+void serialize(std::ostream &out, const Eigen::Vector3f &value)
 {
-  utils::serialize(out, v(0));
-  utils::serialize(out, v(1));
-  utils::serialize(out, v(2));
-
-  return out;
+  utils::serialize(out, value(0));
+  utils::serialize(out, value(1));
+  utils::serialize(out, value(2));
 }
 
-bool deserialize(std::istream &in, Eigen::Vector3f &v)
+void serialize(std::ostream &out, const std::optional<Eigen::Vector3f> &value)
+{
+  const auto hasValue = value.has_value();
+  out.write(reinterpret_cast<const char *>(&hasValue), sizeof(bool));
+  if (hasValue)
+  {
+    serialize(out, *value);
+  }
+}
+
+bool deserialize(std::istream &in, Eigen::Vector3f &value)
 {
   bool ok{true};
-  ok &= utils::deserialize(in, v(0));
-  ok &= utils::deserialize(in, v(1));
-  ok &= utils::deserialize(in, v(2));
+  ok &= utils::deserialize(in, value(0));
+  ok &= utils::deserialize(in, value(1));
+  ok &= utils::deserialize(in, value(2));
 
   return ok;
+}
+
+bool deserialize(std::istream &in, std::optional<Eigen::Vector3f> &value)
+{
+  bool hasValue{false};
+  in.read(reinterpret_cast<char *>(&hasValue), sizeof(bool));
+  if (hasValue)
+  {
+    Eigen::Vector3f raw{};
+    deserialize(in, raw);
+    value = raw;
+  }
+  else
+  {
+    value.reset();
+  }
+
+  return in.good();
 }
 
 } // namespace bsgo
--- src/bsgo/common/VectorUtils.hh
+++ src/bsgo/common/VectorUtils.hh
@@ -2,13 +2,17 @@
 #pragma once
 
 #include <eigen3/Eigen/Eigen>
+#include <optional>
 #include <string>
 
 namespace bsgo {
 
 auto str(const Eigen::Vector3f &v) -> std::string;
 
-auto serialize(std::ostream &out, const Eigen::Vector3f &v) -> std::ostream &;
-bool deserialize(std::istream &in, Eigen::Vector3f &v);
+void serialize(std::ostream &out, const Eigen::Vector3f &value);
+void serialize(std::ostream &out, const std::optional<Eigen::Vector3f> &value);
+
+bool deserialize(std::istream &in, Eigen::Vector3f &value);
+bool deserialize(std::istream &in, std::optional<Eigen::Vector3f> &value);
 
 } // namespace bsgo
