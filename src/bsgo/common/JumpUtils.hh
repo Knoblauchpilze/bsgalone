63cd2fa4722a994704aec369309ace8a62ea1e2a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-18 17:44:19 +0100

    feat: Failure reasons in JumpService and fixed missing functions

--- src/bsgo/common/JumpUtils.hh
+++ src/bsgo/common/JumpUtils.hh
@@ -10,14 +10,18 @@ bool isShipActive(const PlayerShip &ship);
 
 enum class JumpCompletionStatus
 {
-  SHIP_NOT_ACTIVE,
   JUMP_SYSTEM_NOT_SET,
-  SHIP_SYSTEM_NOT_SET,
+  JUMP_SYSTEM_ALREADY_SET,
   JUMPING_TO_SAME_SYSTEM,
   SHIP_IS_DOCKED,
+  SHIP_NOT_ACTIVE,
+  SHIP_SYSTEM_NOT_SET,
   OK
 };
 auto str(const JumpCompletionStatus status) -> std::string;
+
+auto canShipJump(const PlayerShip &ship, const Uuid newSystem) -> JumpCompletionStatus;
 auto canShipCompleteJump(const PlayerShip &ship) -> JumpCompletionStatus;
+auto canShipCancelJump(const PlayerShip &ship) -> JumpCompletionStatus;
 
 } // namespace bsgo
--- src/bsgo/services/JumpService.cc
+++ src/bsgo/services/JumpService.cc
@@ -42,26 +42,6 @@ bool JumpService::tryRegisterJump(const Uuid shipDbId, const Uuid system) const
   return true;
 }
 
-namespace {
-bool canShipCancelJump(const PlayerShip &ship)
-{
-  if (!isShipActive(ship))
-  {
-    return false;
-  }
-  if (!ship.jumpSystem.has_value())
-  {
-    return false;
-  }
-  if (ship.docked)
-  {
-    return false;
-  }
-
-  return true;
-}
-} // namespace
-
 bool JumpService::tryCancelJump(const Uuid shipDbId) const
 {
   const auto maybeShipEntity = m_entityMapper.tryGetShipEntityId(shipDbId);
@@ -70,9 +50,11 @@ bool JumpService::tryCancelJump(const Uuid shipDbId) const
     return false;
   }
 
-  auto ship = m_repositories.playerShipRepository->findOneById(shipDbId);
-  if (!canShipCancelJump(ship))
+  auto ship         = m_repositories.playerShipRepository->findOneById(shipDbId);
+  const auto status = canShipCancelJump(ship);
+  if (JumpCompletionStatus::OK != status)
   {
+    warn("Failed to process jump cancellation for ship " + str(ship.id), str(status));
     return false;
   }
 
@@ -81,10 +63,10 @@ bool JumpService::tryCancelJump(const Uuid shipDbId) const
 
   info("Cancelled jump for ship " + str(shipDbId));
 
-  auto playerShip   = m_coordinator->getEntity(*maybeShipEntity);
-  const auto status = playerShip.statusComp().status();
+  auto playerShip       = m_coordinator->getEntity(*maybeShipEntity);
+  const auto shipStatus = playerShip.statusComp().status();
 
-  const auto newStatus = updateStatusAfterJumpCancellation(status);
+  const auto newStatus = updateStatusAfterJumpCancellation(shipStatus);
   playerShip.statusComp().setStatus(newStatus);
 
   return true;
