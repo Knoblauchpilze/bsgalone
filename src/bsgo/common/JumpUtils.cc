59fe0b8e4db7249670b4a507ffe68e078ec4a4f0
KnoblauchPilze <prolific-programmer@tuta.com>
2024-02-18 17:44:19 +0100

    feat: Failure reasons in JumpService and fixed missing functions

--- src/bsgo/common/JumpUtils.cc
+++ src/bsgo/common/JumpUtils.cc
@@ -12,21 +12,49 @@ auto str(const JumpCompletionStatus status) -> std::string
 {
   switch (status)
   {
-    case JumpCompletionStatus::SHIP_NOT_ACTIVE:
-      return "ship_not_active";
     case JumpCompletionStatus::JUMP_SYSTEM_NOT_SET:
       return "jump_system_not_set";
-    case JumpCompletionStatus::SHIP_SYSTEM_NOT_SET:
-      return "ship_system_not_set";
+    case JumpCompletionStatus::JUMP_SYSTEM_ALREADY_SET:
+      return "jump_system_already_set";
     case JumpCompletionStatus::JUMPING_TO_SAME_SYSTEM:
       return "jumping_to_same_system";
     case JumpCompletionStatus::SHIP_IS_DOCKED:
       return "ship_is_docked";
+    case JumpCompletionStatus::SHIP_NOT_ACTIVE:
+      return "ship_not_active";
+    case JumpCompletionStatus::SHIP_SYSTEM_NOT_SET:
+      return "ship_system_not_set";
     case JumpCompletionStatus::OK:
       return "ok";
     default:
       return "unknown";
   }
+}
+
+auto canShipJump(const PlayerShip &ship, const Uuid newSystem) -> JumpCompletionStatus
+{
+  if (!isShipActive(ship))
+  {
+    return JumpCompletionStatus::SHIP_NOT_ACTIVE;
+  }
+  if (!ship.system.has_value())
+  {
+    return JumpCompletionStatus::SHIP_SYSTEM_NOT_SET;
+  }
+  if (ship.jumpSystem.has_value())
+  {
+    return JumpCompletionStatus::JUMP_SYSTEM_ALREADY_SET;
+  }
+  if (*ship.system == newSystem)
+  {
+    return JumpCompletionStatus::JUMPING_TO_SAME_SYSTEM;
+  }
+  if (ship.docked)
+  {
+    return JumpCompletionStatus::SHIP_IS_DOCKED;
+  }
+
+  return JumpCompletionStatus::OK;
 }
 
 auto canShipCompleteJump(const PlayerShip &ship) -> JumpCompletionStatus
@@ -35,17 +63,39 @@ auto canShipCompleteJump(const PlayerShip &ship) -> JumpCompletionStatus
   {
     return JumpCompletionStatus::SHIP_NOT_ACTIVE;
   }
+  if (!ship.system.has_value())
+  {
+    return JumpCompletionStatus::SHIP_SYSTEM_NOT_SET;
+  }
   if (!ship.jumpSystem.has_value())
   {
     return JumpCompletionStatus::JUMP_SYSTEM_NOT_SET;
+  }
+  if (*ship.jumpSystem == *ship.system)
+  {
+    return JumpCompletionStatus::JUMPING_TO_SAME_SYSTEM;
+  }
+  if (ship.docked)
+  {
+    return JumpCompletionStatus::SHIP_IS_DOCKED;
+  }
+
+  return JumpCompletionStatus::OK;
+}
+
+auto canShipCancelJump(const PlayerShip &ship) -> JumpCompletionStatus
+{
+  if (!isShipActive(ship))
+  {
+    return JumpCompletionStatus::SHIP_NOT_ACTIVE;
   }
   if (!ship.system.has_value())
   {
     return JumpCompletionStatus::SHIP_SYSTEM_NOT_SET;
   }
-  if (*ship.jumpSystem == *ship.system)
+  if (!ship.jumpSystem.has_value())
   {
-    return JumpCompletionStatus::JUMPING_TO_SAME_SYSTEM;
+    return JumpCompletionStatus::JUMP_SYSTEM_NOT_SET;
   }
   if (ship.docked)
   {
