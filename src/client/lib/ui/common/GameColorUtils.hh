26a05e7506a48387d7c78d80721a889b5d578b2a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-25 22:37:08 +0100

    feat: Added a palette composed of useful colors to represent the UI

--- src/client/lib/ui/common/GameColorUtils.hh
+++ src/client/lib/ui/common/GameColorUtils.hh
@@ -3,11 +3,20 @@
 
 #include "Color.hh"
 #include "Faction.hh"
+#include "PlayerView.hh"
 #include <string>
 
 namespace pge {
 
 auto colorFromResourceName(const std::string &name) -> Color;
-auto colorFromFaction(const bsgo::Faction &faction) -> Color;
+auto colorFromFaction(const bsgo::Faction faction) -> Color;
+
+struct ColorPalette
+{
+  Color defaultColor{};
+  Color almostOpaqueColor{};
+};
+
+auto generatePaletteForFaction(const bsgo::Faction faction) -> ColorPalette;
 
 } // namespace pge
