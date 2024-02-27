d61097119c9fb39fc0f0ef05b24001ef610ba905
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-02-27 22:59:27 +0100

    feat: str method in Color

--- src/pge/colors/Color.cc
+++ src/pge/colors/Color.cc
@@ -19,7 +19,41 @@ auto toRgb(const Color &color) -> Color
   auto pixel = toOlcPixel(*color.name);
 
   const RgbData rgb = {.r = pixel.r, .g = pixel.g, .b = pixel.b};
-  return Color{.rgb = rgb, .alpha = color.alpha};
+  return Color{.name = color.name, .rgb = rgb, .alpha = color.alpha};
+}
+
+auto str(const Color &color) -> std::string
+{
+  if (color.name)
+  {
+    return str(*color.name);
+  }
+
+  if (!color.rgb)
+  {
+    return "[no-data]";
+  }
+
+  std::string out{"["};
+
+  out += "r: ";
+  out += std::to_string(color.rgb->r);
+  out += ", ";
+
+  out += "g: ";
+  out += std::to_string(color.rgb->g);
+  out += ", ";
+
+  out += "b: ";
+  out += std::to_string(color.rgb->b);
+  out += ", ";
+
+  out += "a: ";
+  out += std::to_string(color.alpha);
+
+  out += "]";
+
+  return out;
 }
 
 } // namespace pge
