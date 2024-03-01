1a84d19237deffb3509d94fe31cb291b7a0e39b8
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-03-01 15:54:58 +0100

    feat: Picture menu is able to display a texture from a pack

--- src/client/lib/ui/menus/UiPictureMenu.cc
+++ src/client/lib/ui/menus/UiPictureMenu.cc
@@ -6,9 +6,10 @@ namespace pge {
 
 UiPictureMenu::UiPictureMenu(const MenuConfig &config, const PictureConfig &pictureConfig)
   : UiMenu(config, BackgroundConfig{.color = colors::BLANK, .hColor = colors::BLANK})
-  , m_picture(std::make_unique<DecalResource>(pictureConfig.path))
   , m_tint(pictureConfig.tint)
-{}
+{
+  initializeFromConfig(pictureConfig);
+}
 
 void UiPictureMenu::setPictureTint(Color tint)
 {
@@ -20,10 +21,57 @@ void UiPictureMenu::renderCustom(Renderer &engine) const
   const auto iPos = absolutePosition();
   const Vec2f pos{static_cast<float>(iPos.x), static_cast<float>(iPos.y)};
 
+  if (m_picture != nullptr)
+  {
+    renderDecal(engine, pos);
+  }
+  if (m_sprite)
+  {
+    renderSprite(engine.getTextureHandler(), pos);
+  }
+}
+
+void UiPictureMenu::initializeFromConfig(const PictureConfig &config)
+{
+  if (!config.path && !config.texturePack)
+  {
+    error("Failed to initialize picture menu",
+          "Expected at least one of path or texture pack to be provided");
+  }
+
+  if (config.path)
+  {
+    m_picture = std::make_unique<DecalResource>(*config.path);
+  }
+  if (config.texturePack)
+  {
+    SpriteData data{.texturePack = *config.texturePack};
+
+    if (config.spritePos)
+    {
+      data.texturePos = *config.spritePos;
+    }
+
+    m_sprite = data;
+  }
+}
+
+void UiPictureMenu::renderDecal(Renderer &renderer, const Vec2f &pos) const
+{
   const auto size = dims();
   const Vec2f scale{1.0f * size.x / m_picture->w(), 1.0f * size.y / m_picture->h()};
 
-  engine.drawDecal(pos, *m_picture, scale, m_tint);
+  renderer.drawDecal(pos, *m_picture, scale, m_tint);
+}
+
+void UiPictureMenu::renderSprite(sprites::TexturePack &renderer, const Vec2f &pos) const
+{
+  const auto size = dims();
+  const Vec2f scale{1.0f * size.x, 1.0f * size.y};
+
+  sprites::Sprite sprite{.pack = m_sprite->texturePack, .sprite = m_sprite->texturePos};
+
+  renderer.draw(sprite, pos, scale);
 }
 
 } // namespace pge
