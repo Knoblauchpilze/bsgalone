21991e3e8b40cabcaf851e19664954e6871f0df2
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-06-13 18:16:04 +0200

    feat: Wired loading started/finished messages in server

---
+++ src/server/lib/consumers/system/LoadingMessagesConsumer.hh
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include "AbstractMessageConsumer.hh"
+#include "IMessageQueue.hh"
+#include "LoadingFinishedMessage.hh"
+#include "LoadingStartedMessage.hh"
+#include "PlayerListMessage.hh"
+#include "Services.hh"
+
+namespace bsgo {
+
+class LoadingMessagesConsumer : public AbstractMessageConsumer
+{
+  public:
+  LoadingMessagesConsumer(const Services &services, IMessageQueue *const messageQueue);
+  ~LoadingMessagesConsumer() override = default;
+
+  void onMessageReceived(const IMessage &message) override;
+
+  private:
+  IMessageQueue *const m_messageQueue{};
+
+  void forwardLoadingStartedMessage(const LoadingStartedMessage &message) const;
+  void forwardLoadingFinishedMessage(const LoadingFinishedMessage &message) const;
+};
+
+} // namespace bsgo
