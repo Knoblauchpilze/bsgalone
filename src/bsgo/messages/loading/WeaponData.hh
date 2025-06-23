d1930e28305d8700568aaaa3c8fd1ff99c8290a9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-06-23 08:00:14 +0200

    chore: Cosmetics

--- src/bsgo/messages/loading/WeaponData.hh
+++ src/bsgo/messages/loading/WeaponData.hh
@@ -1,12 +1,8 @@
 
 #pragma once
 
-#include "Faction.hh"
-#include "ShipClass.hh"
-#include "Status.hh"
 #include "TimeUtils.hh"
 #include "Uuid.hh"
-#include <eigen3/Eigen/Eigen>
 #include <optional>
 
 namespace bsgo {
@@ -31,5 +27,3 @@ auto serializeWeaponData(std::ostream &out, const WeaponData &data) -> std::ostr
 bool deserializeWeaponData(std::istream &in, WeaponData &data);
 
 } // namespace bsgo
-struct PlayerWeapon
-{};
