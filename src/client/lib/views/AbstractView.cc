d0ff896e271e0be276773f9eb17b052cadc79e83
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-07-18 08:21:05 +0200

    feat: AbstractView subclasses the message consumer class

--- src/client/lib/views/AbstractView.cc
+++ src/client/lib/views/AbstractView.cc
@@ -4,7 +4,12 @@
 namespace pge {
 
 AbstractView::AbstractView(const std::string &name)
-  : core::CoreObject(name)
+  : AbstractView(name, {})
+{}
+
+AbstractView::AbstractView(const std::string &name,
+                           const std::unordered_set<bsgo::MessageType> &relevantMessageTypes)
+  : bsgo::AbstractMessageConsumer(name, relevantMessageTypes)
   , IView()
 {
   setService("view");
@@ -13,6 +18,15 @@ AbstractView::AbstractView(const std::string &name)
 bool AbstractView::isReady() const noexcept
 {
   return true;
+}
+
+void AbstractView::onMessageReceived(const bsgo::IMessage & /*message*/)
+{
+  // Default implementation should never be called. It is provided to conform
+  // to the AbstractMessageConsumer interface but if an inheriting class does
+  // receive some messages, it should also overload this one.
+  // Therefore we raise an error here.
+  error("Please define an overload for this method if you want to receive messages");
 }
 
 } // namespace pge
--- src/client/lib/views/AbstractView.hh
+++ src/client/lib/views/AbstractView.hh
@@ -1,19 +1,23 @@
 
 #pragma once
 
-#include "CoreObject.hh"
+#include "AbstractMessageConsumer.hh"
 #include "IView.hh"
 #include <string>
 
 namespace pge {
 
-class AbstractView : public core::CoreObject, public IView
+class AbstractView : public bsgo::AbstractMessageConsumer, public IView
 {
   public:
   AbstractView(const std::string &name);
+  AbstractView(const std::string &name,
+               const std::unordered_set<bsgo::MessageType> &relevantMessageTypes);
   ~AbstractView() override = default;
 
   bool isReady() const noexcept override;
+
+  void onMessageReceived(const bsgo::IMessage &message) override;
 };
 
 } // namespace pge
