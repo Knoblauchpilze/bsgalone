00b8d75800520eb5097913299628cc24f0d192d5
KnoblauchPilze <prolific-programmer@tuta.com>
2025-07-21 08:13:02 +0200

    feat: ServerView is using the SystemListMessage to initialize itself

--- src/client/lib/views/Views.cc
+++ src/client/lib/views/Views.cc
@@ -17,7 +17,7 @@ auto createViews(const ViewsConfig &config, const bsgo::DatabaseEntityMapper &en
   out.systemView   = std::make_shared<SystemView>(config.coordinator, entityMapper);
   out.playerView   = std::make_shared<PlayerView>(config.repositories, config.outputMessageQueue);
   out.shopView     = std::make_shared<ShopView>(config.repositories);
-  out.serverView   = std::make_shared<ServerView>(config.gameSession, config.repositories);
+  out.serverView   = std::make_shared<ServerView>(config.gameSession);
   out.resourceView = std::make_shared<ResourceView>(config.repositories);
 
   return out;
@@ -34,14 +34,20 @@ void registerViewToQueue(AbstractView &view,
 
 void registerViews(const Views &views, bsgo::IMessageQueue *const queue)
 {
+  std::unordered_set<bsgo::MessageType> messageTypes{};
+
   // TODO: specify the right kind of messages
-  registerViewToQueue(*views.playerView, {}, queue);
-  registerViewToQueue(*views.shipView, {}, queue);
-  registerViewToQueue(*views.shipDbView, {}, queue);
-  registerViewToQueue(*views.shopView, {}, queue);
-  registerViewToQueue(*views.systemView, {}, queue);
-  registerViewToQueue(*views.serverView, {}, queue);
-  registerViewToQueue(*views.resourceView, {}, queue);
+  registerViewToQueue(*views.playerView, messageTypes, queue);
+  registerViewToQueue(*views.shipView, messageTypes, queue);
+  registerViewToQueue(*views.shipDbView, messageTypes, queue);
+  registerViewToQueue(*views.shopView, messageTypes, queue);
+  registerViewToQueue(*views.systemView, messageTypes, queue);
+
+  messageTypes = {bsgo::MessageType::SYSTEM_LIST};
+  registerViewToQueue(*views.serverView, messageTypes, queue);
+
+  messageTypes = {};
+  registerViewToQueue(*views.resourceView, messageTypes, queue);
 }
 
 } // namespace pge
