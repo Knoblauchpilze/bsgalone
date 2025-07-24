68893565aba2345bc5d4702b84e47de03fd72249
KnoblauchPilze <prolific-programmer@tuta.com>
2025-07-24 08:18:42 +0200

    feat: ResourceView is using the ResourceListMessage

--- src/client/lib/views/ResourceView.cc
+++ src/client/lib/views/ResourceView.cc
@@ -1,16 +1,39 @@
 
 #include "ResourceView.hh"
+#include "ResourceListMessage.hh"
 
 namespace pge {
 
-ResourceView::ResourceView(const bsgo::Repositories &repositories)
+ResourceView::ResourceView()
   : AbstractView("resource")
-  , m_repositories(repositories)
 {}
+
+bool ResourceView::isReady() const noexcept
+{
+  return !m_resources.empty();
+}
+
+void ResourceView::onMessageReceived(const bsgo::IMessage &message)
+{
+  const auto resourceList = message.as<bsgo::ResourceListMessage>();
+  const auto resources    = resourceList.getResourcesData();
+
+  for (const auto &resource : resources)
+  {
+    m_resources.insert({resource.dbId, resource});
+  }
+}
 
 auto ResourceView::getResourceName(const bsgo::Uuid resource) const -> std::string
 {
-  return m_repositories.resourceRepository->findOneById(resource).name;
+  const auto maybeResource = m_resources.find(resource);
+
+  if (maybeResource == m_resources.end())
+  {
+    error("Failed to get resource", "No resource registered for id " + bsgo::str(resource));
+  }
+
+  return maybeResource->second.name;
 }
 
 } // namespace pge
