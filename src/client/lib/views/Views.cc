977f4b23f571abbf65b455b186c390a2242780e9
Maxime Rigaud <maxime.rigaud@sumup.com>
2025-08-21 07:57:07 +0200

    feat: ShopView receives the player resources

--- src/client/lib/views/Views.cc
+++ src/client/lib/views/Views.cc
@@ -57,7 +57,8 @@ void registerViews(const Views &views, bsgo::IMessageQueue *const queue)
 
   messageTypes = {bsgo::MessageType::RESOURCE_LIST,
                   bsgo::MessageType::COMPUTER_LIST,
-                  bsgo::MessageType::WEAPON_LIST};
+                  bsgo::MessageType::WEAPON_LIST,
+                  bsgo::MessageType::PLAYER_RESOURCE_LIST};
   registerViewToQueue(*views.shopView, messageTypes, queue);
 
   messageTypes = {};
