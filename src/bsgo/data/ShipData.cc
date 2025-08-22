1afacd017f90f6c58f6b17846fe9d1aa448e5f8f
Maxime Rigaud <maxime.rigaud@sumup.com>
2025-08-22 08:26:16 +0200

    feat: ShipData contains price information

--- src/bsgo/data/ShipData.cc
+++ src/bsgo/data/ShipData.cc
@@ -8,4 +8,33 @@ bool ShipData::operator==(const ShipData &rhs) const
   return dbId == rhs.dbId;
 }
 
+auto fromDbShip(const Ship &ship, const ShipPriceRepository &repository) -> ShipData
+{
+  ShipData out{
+    .dbId            = ship.id,
+    .faction         = ship.faction,
+    .shipClass       = ship.shipClass,
+    .name            = ship.name,
+    .maxHullPoints   = ship.maxHullPoints,
+    .hullPointsRegen = ship.hullPointsRegen,
+    .maxPowerPoints  = ship.maxPowerPoints,
+    .powerRegen      = ship.powerRegen,
+    .acceleration    = ship.acceleration,
+    .speed           = ship.speed,
+    .radius          = ship.radius,
+
+    .jumpTime         = ship.jumpTime,
+    .jumpTimeInThreat = ship.jumpTimeInThreat,
+
+    .slots = ship.slots,
+  };
+
+  for (const auto &[resource, cost] : repository.findAllByShip(ship.id))
+  {
+    out.price[resource] = cost;
+  }
+
+  return out;
+}
+
 } // namespace bsgo
