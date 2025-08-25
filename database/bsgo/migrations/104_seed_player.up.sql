8571b09a23d2386e93ca052355533b76f8eee99d
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-08-25 08:19:15 +0200

    chore: Structured seeding in a readable way

---
+++ database/bsgo/migrations/104_seed_player.up.sql
@@ -0,0 +1,118 @@
+
+-- player: color
+INSERT INTO player ("name", "password", "faction")
+  VALUES ('colo', 'aze', 'colonial');
+
+INSERT INTO player_resource ("player", "resource", "amount")
+  VALUES (
+    (SELECT id FROM player WHERE name = 'colo'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    100501.2
+  );
+INSERT INTO player_resource ("player", "resource", "amount")
+  VALUES (
+    (SELECT id FROM player WHERE name = 'colo'),
+    (SELECT id FROM resource WHERE name = 'titane'),
+    1017.2
+  );
+
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Medium range cannon'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    2
+  );
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Short range cannon'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    1
+  );
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Long range cannon'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    6
+  );
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Long range cannon'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    7
+  );
+
+INSERT INTO player_computer ("computer", "player", "level")
+  VALUES (
+    (SELECT id FROM computer WHERE name = 'Weapon buff'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    1
+  );
+INSERT INTO player_computer ("computer", "player", "level")
+  VALUES (
+    (SELECT id FROM computer WHERE name = 'Scan'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    5
+  );
+INSERT INTO player_computer ("computer", "player", "level")
+  VALUES (
+    (SELECT id FROM computer WHERE name = 'Weapon buff'),
+    (SELECT id FROM player WHERE name = 'colo'),
+    6
+  );
+
+-- player: colo2
+INSERT INTO player ("name", "password", "faction")
+  VALUES ('colo2', 'aze', 'colonial');
+
+INSERT INTO player_resource ("player", "resource", "amount")
+  VALUES (
+    (SELECT id FROM player WHERE name = 'colo2'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    4567.0
+  );
+
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Long range cannon'),
+    (SELECT id FROM player WHERE name = 'colo2'),
+    8
+  );
+
+INSERT INTO player_computer ("computer", "player", "level")
+  VALUES (
+    (SELECT id FROM computer WHERE name = 'Scan'),
+    (SELECT id FROM player WHERE name = 'colo2'),
+    5
+  );
+
+-- player: toast
+INSERT INTO player ("name", "password", "faction")
+  VALUES ('toast', 'aze', 'cylon');
+
+INSERT INTO player_resource ("player", "resource", "amount")
+  VALUES (
+    (SELECT id FROM player WHERE name = 'toast'),
+    (SELECT id FROM resource WHERE name = 'tylium'),
+    1234.2
+  );
+INSERT INTO player_resource ("player", "resource", "amount")
+  VALUES (
+    (SELECT id FROM player WHERE name = 'toast'),
+    (SELECT id FROM resource WHERE name = 'titane'),
+    56789.2
+  );
+
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Medium range cannon'),
+    (SELECT id FROM player WHERE name = 'toast'),
+    3
+  );
+
+--- player: AI
+INSERT INTO player_weapon ("weapon", "player", "level")
+  VALUES (
+    (SELECT id FROM weapon WHERE name = 'Medium range cannon'),
+    NULL,
+    20
+  );
--- database/bsgo/migrations/2_create_types.up.sql
+++ database/bsgo/migrations/2_create_types.up.sql
@@ -20,16 +20,3 @@ CREATE TABLE slot (
   type TEXT NOT NULL,
   PRIMARY KEY (type)
 );
-
-INSERT INTO faction ("name") VALUES ('cylon');
-INSERT INTO faction ("name") VALUES ('colonial');
-
-INSERT INTO entity ("name") VALUES ('ship');
-INSERT INTO entity ("name") VALUES ('asteroid');
-INSERT INTO entity ("name") VALUES ('outpost');
-
-INSERT INTO resource ("name") VALUES ('tylium');
-INSERT INTO resource ("name") VALUES ('titane');
-
-INSERT INTO slot ("type") VALUES ('weapon');
-INSERT INTO slot ("type") VALUES ('computer');
