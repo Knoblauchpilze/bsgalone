7032d2929ccb709319f0d2fe09948b17af47ffcf
KnoblauchPilze <prolific-programmer@tuta.com>
2025-09-06 11:34:47 +0200

    feat: Asteroids have an id and can be updated

--- src/bsgo/repositories/AsteroidRepository.cc
+++ src/bsgo/repositories/AsteroidRepository.cc
@@ -14,12 +14,22 @@ constexpr auto FIND_ONE_QUERY
 
 constexpr auto FIND_LOOT_QUERY_NAME = "asteroid_find_loot";
 constexpr auto FIND_LOOT_QUERY = "SELECT count(resource) FROM asteroid_loot WHERE asteroid = $1";
+
+constexpr auto UPDATE_ASTEROID_QUERY_NAME = "asteroid_update";
+constexpr auto UPDATE_ASTEROID_QUERY      = R"(
+UPDATE asteroid
+  SET
+    health = $1
+  WHERE
+    id = $2
+)";
 } // namespace
 
 void AsteroidRepository::initialize()
 {
   m_connection->prepare(FIND_ONE_QUERY_NAME, FIND_ONE_QUERY);
   m_connection->prepare(FIND_LOOT_QUERY_NAME, FIND_LOOT_QUERY);
+  m_connection->prepare(UPDATE_ASTEROID_QUERY_NAME, UPDATE_ASTEROID_QUERY);
 }
 
 auto AsteroidRepository::findOneById(const Uuid asteroid) const -> Asteroid
@@ -28,6 +38,22 @@ auto AsteroidRepository::findOneById(const Uuid asteroid) const -> Asteroid
   fetchLoot(asteroid, out);
 
   return out;
+}
+
+void AsteroidRepository::save(const Asteroid &asteroid)
+{
+  auto query = [&asteroid](pqxx::work &transaction) {
+    return transaction
+      .exec(pqxx::prepped{UPDATE_ASTEROID_QUERY_NAME},
+            pqxx::params{asteroid.health, toDbId(asteroid.id)})
+      .no_rows();
+  };
+
+  const auto res = m_connection->tryExecuteTransaction(query);
+  if (res.error)
+  {
+    error("Failed to save asteroid: " + *res.error);
+  }
 }
 
 auto AsteroidRepository::fetchAsteroidBase(const Uuid asteroid) const -> Asteroid
@@ -39,6 +65,7 @@ auto AsteroidRepository::fetchAsteroidBase(const Uuid asteroid) const -> Asteroi
 
   Asteroid out;
 
+  out.id        = asteroid;
   out.system    = fromDbId(record[0].as<int>());
   out.health    = record[1].as<float>();
   out.maxHealth = record[2].as<float>();
