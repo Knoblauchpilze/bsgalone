2da91256bb453a9027f40cfb9197d75b892b6b01
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-09-07 14:34:59 +0200

    feat: Added DB sync process for ships

--- src/server/lib/game/processes/database/PlayerShipSynchronizer.cc
+++ src/server/lib/game/processes/database/PlayerShipSynchronizer.cc
@@ -8,15 +8,12 @@ void syncPlayerShip(const Entity &entity, const Repositories &repositories)
   const auto dbId = entity.dbComp().dbId();
 
   PlayerShip ship{
-    .id = dbId,
-    // TODO: Correctly fetch this information
-    // We also need the jump system
-    .ship        = 0,
+    .id          = dbId,
     .name        = entity.nameComp().name(),
-    .player      = 0,
     .active      = true,
     .hullPoints  = entity.healthComp().value(),
     .powerPoints = entity.powerComp().value(),
+    // TODO: We need to fetch the jump system
   };
 
   repositories.playerShipRepository->save(ship);
