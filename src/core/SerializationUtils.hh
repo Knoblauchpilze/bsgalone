29478499c3f7ea4f8b24cebd87825708d26b6d00
KnoblauchPilze <prolific-programmer@tuta.com>
2025-09-26 16:17:57 +0200

    chore: Cleaned-up serialization and added concepts for de/serializable

--- src/core/SerializationUtils.hh
+++ src/core/SerializationUtils.hh
@@ -10,56 +10,58 @@
 
 namespace core {
 
-/// https://stackoverflow.com/questions/55647741/template-specialization-with-enable-if
-/// https://en.cppreference.com/w/cpp/types/enable_if
-
-template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
-auto serialize(std::ostream &out, const T &e) -> std::ostream &;
-
-template<typename T, std::enable_if_t<!std::is_enum<T>::value, bool> = true>
-auto serialize(std::ostream &out, const T &e) -> std::ostream &;
-
-template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
-bool deserialize(std::istream &in, T &e);
-
-template<typename T, std::enable_if_t<!std::is_enum<T>::value, bool> = true>
-bool deserialize(std::istream &in, T &e);
-
 auto serialize(std::ostream &out, const std::string &str) -> std::ostream &;
-
 bool deserialize(std::istream &in, std::string &str);
 
-template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
-auto serialize(std::ostream &out, const std::optional<T> &value) -> std::ostream &;
-
-template<typename T, std::enable_if_t<!std::is_enum<T>::value, bool> = true>
-auto serialize(std::ostream &out, const std::optional<T> &value) -> std::ostream &;
-
-template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
-bool deserialize(std::istream &in, std::optional<T> &value);
-
-template<typename T, std::enable_if_t<!std::is_enum<T>::value, bool> = true>
-bool deserialize(std::istream &in, std::optional<T> &value);
-
-template<typename Key, std::enable_if_t<std::is_enum<Key>::value, bool> = true>
-auto serialize(std::ostream &out, const std::unordered_map<Key, int> &m) -> std::ostream &;
-
-template<typename Key, std::enable_if_t<!std::is_enum<Key>::value, bool> = true>
-auto serialize(std::ostream &out, const std::unordered_map<Key, int> &m) -> std::ostream &;
-
-template<typename Key, std::enable_if_t<std::is_enum<Key>::value, bool> = true>
-bool deserialize(std::istream &in, std::unordered_map<Key, int> &m);
-
-template<typename Key, std::enable_if_t<!std::is_enum<Key>::value, bool> = true>
-bool deserialize(std::istream &in, std::unordered_map<Key, int> &m);
+auto serialize(std::ostream &out, const Eigen::Vector3f &v) -> std::ostream &;
+bool deserialize(std::istream &in, Eigen::Vector3f &v);
 
 auto serialize(std::ostream &out, const core::Duration &d) -> std::ostream &;
-
 bool deserialize(std::istream &in, core::Duration &d);
 
-auto serialize(std::ostream &out, const Eigen::Vector3f &v) -> std::ostream &;
+template<typename T>
+auto serialize(std::ostream &out, const std::optional<T> &value) -> std::ostream &;
+template<typename T>
+bool deserialize(std::istream &in, std::optional<T> &value);
 
-bool deserialize(std::istream &in, Eigen::Vector3f &v);
+template<typename Key, typename Value>
+auto serialize(std::ostream &out, const std::unordered_map<Key, int> &m) -> std::ostream &;
+template<typename Key, typename Value>
+bool deserialize(std::istream &in, std::unordered_map<Key, int> &m);
+
+/// https://cplusplus.com/forum/general/285613/
+/// https://en.cppreference.com/w/cpp/language/constraints.html
+template<typename T>
+concept serializable = requires(T t, std::ostream &out)
+{
+  {
+    t.serialize(out)
+    } -> std::convertible_to<std::ostream &>;
+};
+
+template<typename T>
+concept deserializable = requires(T t, std::istream &in)
+{
+  {
+    t.deserialize(in)
+    } -> std::convertible_to<bool>;
+};
+
+template<serializable T>
+auto serialize(std::ostream &out, const T &e) -> std::ostream &;
+
+template<deserializable T>
+bool deserialize(std::istream &in, T &e);
+
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true>
+auto serialize(std::ostream &out, const T &e) -> std::ostream &;
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true>
+bool deserialize(std::istream &in, T &e);
+
+template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
+auto serialize(std::ostream &out, const T &e) -> std::ostream &;
+template<typename T, std::enable_if_t<std::is_enum<T>::value, bool> = true>
+bool deserialize(std::istream &in, T &e);
 
 } // namespace core
 
