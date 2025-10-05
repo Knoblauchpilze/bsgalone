0844ab0616dec5a3d892a6c84f45921f90b9267c
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-10-05 15:06:10 +0200

    feat: Added Duration class

---
+++ src/bsgo/time/Duration.hh
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <string>
+
+namespace bsgo {
+
+/// @brief - Represents a duration in the game. Such a duration does not have a unit,
+/// it merely represents the time in the game. It is consistent with the `Tick` class
+/// as the addition between a `Tick` and a `Duration` is well defined.
+/// A duration cannot be negative: this is enforced when constructing a new instance
+/// of the class.
+class Duration
+{
+  public:
+  /// @brief - Creates a new duration from the input floating point value. The value
+  /// cannot be negative: if this is the case an exception will be raised.
+  /// @param elapsed - the duration to represent.
+  explicit Duration(const float elapsed);
+
+  auto str() const -> std::string;
+
+  private:
+  float m_elapsed{0.0f};
+
+  void validate();
+};
+
+} // namespace bsgo
---
+++ tests/unit/bsgo/time/DurationTest.cc
@@ -0,0 +1,18 @@
+
+#include "Duration.hh"
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+namespace bsgo {
+TEST(Unit_Bsgo_Duration, CreatesPositiveDuration)
+{
+  Duration d(1.45f);
+  EXPECT_EQ(d.str(), std::string("1.450000"));
+}
+
+TEST(Unit_Bsgo_Duration, ThrowsWhenIntIsNegative)
+{
+  EXPECT_THROW([] { Duration(-0.47f); }(), std::invalid_argument);
+}
+} // namespace bsgo
