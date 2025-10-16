b33eb8f3f9857222dc85bafd095902e0513d7982
KnoblauchPilze <prolific-programmer@tuta.com>
2025-10-16 21:58:27 +0200

    feat: Added fromString method to create a time unit

--- src/time/Unit.hh
+++ src/time/Unit.hh
@@ -14,4 +14,6 @@ enum class Unit
 auto str(const Unit unit) -> std::string;
 auto asTimeString(const Unit unit) -> std::string;
 
+auto fromString(const std::string_view unit) -> Unit;
+
 } // namespace chrono
