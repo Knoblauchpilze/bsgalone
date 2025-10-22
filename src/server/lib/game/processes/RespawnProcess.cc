4c5b51628ca46be0281c5a9780ed94aafbb02ed7
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-10-22 08:21:06 +0200

    feat: Respawn process sends an entity added message to the system processor

--- src/server/lib/game/processes/RespawnProcess.cc
+++ src/server/lib/game/processes/RespawnProcess.cc
@@ -1,5 +1,6 @@
 
 #include "RespawnProcess.hh"
+#include "EntityAddedMessage.hh"
 
 namespace bsgo {
 
@@ -30,6 +31,13 @@ void RespawnProcess::respawnAsteroid(Coordinator & /*coordinator*/, Asteroid ast
   info("Respawning asteroid with " + std::to_string(asteroid.health) + " health");
   m_repositories.asteroidRepository->save(asteroid);
   m_repositories.asteroidRepository->deleteRespawn(asteroid.id);
+
+  // TODO: This will crash in the EntityAddedMessageConsumer as it does
+  // not know how to handle asteroids.
+  auto added = std::make_unique<EntityAddedMessage>(asteroid.system);
+  AsteroidData data{.dbId = asteroid.id};
+  added->setAsteroidData(data);
+  m_systemMessageQueue->pushMessage(std::move(added));
 }
 
 } // namespace bsgo
