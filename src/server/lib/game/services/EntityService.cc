7b5556c55c4616e4c50677dfd4537ad17e9df9ac
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-10-23 07:57:40 +0200

    feat: Handled respawn of asteroids

--- src/server/lib/game/services/EntityService.cc
+++ src/server/lib/game/services/EntityService.cc
@@ -65,6 +65,22 @@ void EntityService::tryDeleteShipEntity(const Uuid shipDbId) const
   handlePlayerDeletionForShip(shipDbId);
 }
 
+bool EntityService::tryCreateAsteroidEntity(const Uuid asteroidDbId) const
+{
+  AsteroidDataSource source{m_repositories};
+  source.registerAsteroid(*m_coordinator, asteroidDbId, m_entityMapper);
+
+  const auto maybeEntityId = m_entityMapper.tryGetAsteroidEntityId(asteroidDbId);
+  if (!maybeEntityId)
+  {
+    warn("Failed to create entity for asteroid " + str(asteroidDbId),
+         "Registration did not create an entity for it");
+    return false;
+  }
+
+  return true;
+}
+
 void EntityService::tryDeleteAsteroidEntity(const Uuid asteroidDbId) const
 {
   const auto maybeEntityId = m_entityMapper.tryGetAsteroidEntityId(asteroidDbId);
