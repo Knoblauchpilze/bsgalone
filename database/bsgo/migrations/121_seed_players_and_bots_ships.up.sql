2ade2fd29b82336dd2a2fe8e5b7f68198fec9c59
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-10-28 19:16:34 +0100

    chore: Used proper aliases in seeding queries

--- database/bsgo/migrations/121_seed_players_and_bots_ships.up.sql
+++ database/bsgo/migrations/121_seed_players_and_bots_ships.up.sql
@@ -14,33 +14,33 @@ INSERT INTO player_ship ("ship", "player", "name", "active", "hull_points", "pow
   );
 INSERT INTO ship_system ("ship", "system", "docked")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = 'colo' AND player_ship.active = true),
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player WHERE p.name = 'colo' AND ps.active = true),
     (SELECT id FROM system WHERE name = 'Munnin'),
     true
   );
 
 INSERT INTO ship_weapon ("ship", "weapon", "slot")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo' AND ship.name = 'Viper Mark II'),
-    (SELECT player_weapon.id FROM player_weapon LEFT JOIN player ON player.id = player_weapon.player LEFT JOIN weapon ON player_weapon.weapon = weapon.id WHERE player.name = 'colo' AND weapon.name = 'Medium range cannon'),
-    (SELECT ship_slot.id FROM ship_slot LEFT JOIN ship ON ship_slot.ship = ship.id WHERE ship.name = 'Viper Mark II' AND ship_slot.type = 'weapon' AND ship_slot.x_pos = -0.5)
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo' AND s.name = 'Viper Mark II'),
+    (SELECT pw.id FROM player_weapon AS pw LEFT JOIN player AS p ON p.id = pw.player LEFT JOIN weapon AS w ON w.id = pw.weapon WHERE p.name = 'colo' AND w.name = 'Medium range cannon'),
+    (SELECT ss.id FROM ship_slot AS ss LEFT JOIN ship AS s ON s.id = ss.ship WHERE s.name = 'Viper Mark II' AND ss.type = 'weapon' AND ss.x_pos = -0.5)
   );
 INSERT INTO ship_weapon ("ship", "weapon", "slot")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo' AND ship.name = 'Viper Mark II'),
-    (SELECT player_weapon.id FROM player_weapon LEFT JOIN player ON player.id = player_weapon.player LEFT JOIN weapon ON player_weapon.weapon = weapon.id WHERE player.name = 'colo' AND weapon.name = 'Short range cannon'),
-    (SELECT ship_slot.id FROM ship_slot LEFT JOIN ship ON ship_slot.ship = ship.id WHERE ship.name = 'Viper Mark II' AND ship_slot.type = 'weapon' AND ship_slot.x_pos = 0.0)
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo' AND s.name = 'Viper Mark II'),
+    (SELECT pw.id FROM player_weapon AS pw LEFT JOIN player AS p ON p.id = pw.player LEFT JOIN weapon AS w ON w.id = pw.weapon WHERE p.name = 'colo' AND w.name = 'Short range cannon'),
+    (SELECT ss.id FROM ship_slot AS ss LEFT JOIN ship AS s ON s.id = ss.ship WHERE s.name = 'Viper Mark II' AND ss.type = 'weapon' AND ss.x_pos = 0.0)
   );
 
 INSERT INTO ship_computer ("ship", "computer")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo' AND ship.name = 'Viper Mark II'),
-    (SELECT player_computer.id FROM player_computer LEFT JOIN player ON player.id = player_computer.player LEFT JOIN computer ON player_computer.computer = computer.id WHERE player.name = 'colo' AND computer.name = 'Weapon buff' LIMIT 1)
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo' AND s.name = 'Viper Mark II'),
+    (SELECT pc.id FROM player_computer AS pc LEFT JOIN player AS p ON p.id = pc.player LEFT JOIN computer AS c ON c.id = pc.computer WHERE p.name = 'colo' AND c.name = 'Weapon buff' LIMIT 1)
   );
 INSERT INTO ship_computer ("ship", "computer")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo' AND ship.name = 'Viper Mark II'),
-    (SELECT player_computer.id FROM player_computer LEFT JOIN player ON player.id = player_computer.player LEFT JOIN computer ON player_computer.computer = computer.id WHERE player.name = 'colo' AND computer.name = 'Scan')
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo' AND s.name = 'Viper Mark II'),
+    (SELECT pc.id FROM player_computer AS pc LEFT JOIN player AS p ON p.id = pc.player LEFT JOIN computer AS c ON c.id = pc.computer WHERE p.name = 'colo' AND c.name = 'Scan')
   );
 
 -- player: colo, ship: Jotunn
@@ -72,22 +72,22 @@ INSERT INTO player_ship ("ship", "player", "name", "active", "hull_points", "pow
   );
 INSERT INTO ship_system ("ship", "system", "docked")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = 'colo2' AND player_ship.active = true),
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player WHERE p.name = 'colo2' AND ps.active = true),
     (SELECT id FROM system WHERE name = '169 aretis'),
     true
   );
 
 INSERT INTO ship_weapon ("ship", "weapon", "slot")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo2' AND ship.name = 'Viper Mark VII'),
-    (SELECT player_weapon.id FROM player_weapon LEFT JOIN player ON player.id = player_weapon.player LEFT JOIN weapon ON player_weapon.weapon = weapon.id WHERE player.name = 'colo2' AND weapon.name = 'Long range cannon'),
-    (SELECT ship_slot.id FROM ship_slot LEFT JOIN ship ON ship_slot.ship = ship.id WHERE ship.name = 'Viper Mark II' AND ship_slot.type = 'weapon' AND ship_slot.x_pos = 0.0)
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo2' AND s.name = 'Viper Mark VII'),
+    (SELECT pw.id FROM player_weapon AS pw LEFT JOIN player AS p ON p.id = pw.player LEFT JOIN weapon AS w ON w.id = pw.weapon WHERE p.name = 'colo2' AND w.name = 'Long range cannon'),
+    (SELECT ss.id FROM ship_slot AS ss LEFT JOIN ship AS s ON s.id = ss.ship WHERE s.name = 'Viper Mark II' AND ss.type = 'weapon' AND ss.x_pos = 0.0)
   );
 
 INSERT INTO ship_computer ("ship", "computer")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'colo2' AND ship.name = 'Viper Mark VII'),
-    (SELECT player_computer.id FROM player_computer LEFT JOIN player ON player.id = player_computer.player LEFT JOIN computer ON player_computer.computer = computer.id WHERE player.name = 'colo2' AND computer.name = 'Scan')
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'colo2' AND s.name = 'Viper Mark VII'),
+    (SELECT pc.id FROM player_computer AS pc LEFT JOIN player AS p ON p.id = pc.player LEFT JOIN computer AS c ON c.id = pc.computer WHERE p.name = 'colo2' AND c.name = 'Scan')
   );
 
 -- player: toast, ship: Cylon Raider, system: Munnin
@@ -105,16 +105,16 @@ INSERT INTO player_ship ("ship", "player", "name", "active", "hull_points", "pow
   );
 INSERT INTO ship_system ("ship", "system", "docked")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player_ship.player = player.id WHERE player.name = 'toast' AND player_ship.active = true),
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player WHERE p.name = 'toast' AND ps.active = true),
     (SELECT id FROM system WHERE name = 'Munnin'),
     true
   );
 
 INSERT INTO ship_weapon ("ship", "weapon", "slot")
   VALUES (
-    (SELECT player_ship.id FROM player_ship LEFT JOIN player ON player.id = player_ship.player LEFT JOIN ship ON player_ship.ship = ship.id WHERE player.name = 'toast' AND ship.name = 'Cylon Raider'),
-    (SELECT player_weapon.id FROM player_weapon LEFT JOIN player ON player.id = player_weapon.player LEFT JOIN weapon ON player_weapon.weapon = weapon.id WHERE player.name = 'toast' AND weapon.name = 'Medium range cannon'),
-    (SELECT ship_slot.id FROM ship_slot LEFT JOIN ship ON ship_slot.ship = ship.id WHERE ship.name = 'Viper Mark II' AND ship_slot.type = 'weapon' AND ship_slot.x_pos = 0.0)
+    (SELECT ps.id FROM player_ship AS ps LEFT JOIN player AS p ON p.id = ps.player LEFT JOIN ship AS s ON s.id = ps.ship WHERE p.name = 'toast' AND s.name = 'Cylon Raider'),
+    (SELECT pw.id FROM player_weapon AS pw LEFT JOIN player AS p ON p.id = pw.player LEFT JOIN weapon AS w ON w.id = pw.weapon WHERE p.name = 'toast' AND w.name = 'Medium range cannon'),
+    (SELECT ss.id FROM ship_slot AS ss LEFT JOIN ship AS s ON s.id = ss.ship WHERE s.name = 'Viper Mark II' AND ss.type = 'weapon' AND ss.x_pos = 0.0)
   );
 
 -- player: munnin_cylon_war_raider, ship: Cylon Raider, system: Munnin
@@ -134,7 +134,7 @@ INSERT INTO player_ship ("ship", "player", "name", "active", "hull_points", "pow
 INSERT INTO ship_system ("ship", "system", "docked")
   VALUES (
     -- TODO: Should be replaced with the actual player id
-    (SELECT player_ship.id FROM player_ship WHERE player_ship.player is NULL),
+    (SELECT id FROM player_ship WHERE player is NULL),
     (SELECT id FROM system WHERE name = 'Munnin'),
     false
   );
@@ -142,8 +142,8 @@ INSERT INTO ship_system ("ship", "system", "docked")
 INSERT INTO ship_weapon ("ship", "weapon", "slot")
   VALUES (
     (SELECT id FROM player_ship WHERE player is NULL),
-    (SELECT player_weapon.id FROM player_weapon LEFT JOIN weapon ON player_weapon.weapon = weapon.id WHERE player_weapon.player is NULL AND weapon.name = 'Medium range cannon'),
-    (SELECT ship_slot.id FROM ship_slot LEFT JOIN ship ON ship_slot.ship = ship.id WHERE ship.name = 'Cylon Raider' AND ship_slot.type = 'weapon' AND ship_slot.x_pos = -0.5)
+    (SELECT pw.id FROM player_weapon AS pw LEFT JOIN weapon AS w ON w.id = pw.weapon WHERE pw.player is NULL AND w.name = 'Medium range cannon'),
+    (SELECT ss.id FROM ship_slot AS ss LEFT JOIN ship AS s ON s.id = ss.ship WHERE s.name = 'Cylon Raider' AND ss.type = 'weapon' AND ss.x_pos = -0.5)
   );
 
 INSERT INTO ai_targets ("ship", "index", "x_pos", "y_pos", "z_pos")
