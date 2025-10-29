a06cb6b8bb00dc83793659c9e9baeb88260b4d1c
KnoblauchPilze <prolific-programmer@tuta.com>
2025-10-29 19:18:03 +0100

    chore: PlayerResourceData serializes itself

--- src/bsgo/data/PlayerResourceData.hh
+++ src/bsgo/data/PlayerResourceData.hh
@@ -14,6 +14,9 @@ struct PlayerResourceData
   int amount{};
 
   bool operator==(const PlayerResourceData &rhs) const;
+
+  auto serialize(std::ostream &out) const -> std::ostream &;
+  bool deserialize(std::istream &in);
 };
 
 auto fromDbPlayerResource(const PlayerResource &playerResource) -> PlayerResourceData;
