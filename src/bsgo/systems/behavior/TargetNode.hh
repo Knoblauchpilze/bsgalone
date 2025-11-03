6c2ce40133ea95baf3af573332b8c0eadfa40fb3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-11-03 08:21:27 +0100

    feat: Added DataContext as argument of the behavior tree nodes and registered changes to the target

--- src/bsgo/systems/behavior/TargetNode.hh
+++ src/bsgo/systems/behavior/TargetNode.hh
@@ -9,14 +9,19 @@ namespace bsgo {
 class TargetNode : public LeafNode
 {
   public:
-  TargetNode(const Eigen::Vector3f &target);
+  /// @brief - Creates a new target node which will try to reach a certain position.
+  /// @param target - the position to reach
+  /// @param index - used for reporting to update the data context when this node is
+  /// first activated.
+  TargetNode(const Eigen::Vector3f &target, const int index);
   ~TargetNode() override = default;
 
   protected:
   void run(const BehaviorData &data) override;
 
   private:
-  Eigen::Vector3f m_target;
+  int m_index{};
+  Eigen::Vector3f m_target{};
 };
 
 } // namespace bsgo
---
+++ src/bsgo/systems/tree/DataContext.cc
@@ -0,0 +1,32 @@
+
+#include "DataContext.hh"
+
+namespace bsgo {
+
+DataContext::DataContext() {}
+
+auto DataContext::tryGetTargetIndex() const -> std::optional<int>
+{
+  return m_targetIndex;
+}
+
+void DataContext::setTargetIndex(const int targetIndex)
+{
+  if (!m_targetIndex || *m_targetIndex != targetIndex)
+  {
+    m_changed     = true;
+    m_targetIndex = targetIndex;
+  }
+}
+
+bool DataContext::changed() const
+{
+  return m_changed;
+}
+
+void DataContext::markAsSynced()
+{
+  m_changed = false;
+}
+
+} // namespace bsgo
