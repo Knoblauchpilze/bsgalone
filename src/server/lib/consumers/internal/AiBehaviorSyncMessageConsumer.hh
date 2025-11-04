5237328b439342418621b73218f2b2a57b354057
KnoblauchPilze <prolific-programmer@tuta.com>
2025-11-04 08:04:37 +0100

    feat: AI system sends a message received by an internal listener

---
+++ src/server/lib/consumers/internal/AiBehaviorSyncMessageConsumer.hh
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "AbstractMessageConsumer.hh"
+#include "IMessageQueue.hh"
+#include "SystemService.hh"
+
+namespace bsgo {
+
+class AiBehaviorSyncMessageConsumer : public AbstractMessageConsumer
+{
+  public:
+  AiBehaviorSyncMessageConsumer(SystemServiceShPtr systemService,
+                                IMessageQueue *const outputMessageQueue);
+  ~AiBehaviorSyncMessageConsumer() override = default;
+
+  void onMessageReceived(const IMessage &message) override;
+
+  private:
+  SystemServiceShPtr m_systemService{};
+  IMessageQueue *const m_outputMessageQueue{};
+};
+
+} // namespace bsgo
