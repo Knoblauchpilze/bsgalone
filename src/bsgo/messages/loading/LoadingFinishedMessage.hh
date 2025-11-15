246a79b9c5038b6bd05d155dacdc93c4eddf3477
Maxime Rigaud <maxime.rigaud@sumup.com>
2025-11-15 18:17:57 +0100

    chore: LoadingFinishedMessage requires a player identifier

--- src/bsgo/messages/loading/LoadingFinishedMessage.hh
+++ src/bsgo/messages/loading/LoadingFinishedMessage.hh
@@ -11,15 +11,14 @@ class LoadingFinishedMessage : public NetworkMessage
 {
   public:
   LoadingFinishedMessage();
-  LoadingFinishedMessage(const LoadingTransition transition);
+  LoadingFinishedMessage(const LoadingTransition transition, const Uuid playerDbId);
   ~LoadingFinishedMessage() override = default;
 
   auto getTransition() const -> LoadingTransition;
   auto tryGetSystemDbId() const -> std::optional<Uuid>;
-  auto tryGetPlayerDbId() const -> std::optional<Uuid>;
+  auto getPlayerDbId() const -> Uuid;
 
   void setSystemDbId(const Uuid systemDbId);
-  void setPlayerDbId(const Uuid playerDbId);
 
   auto serialize(std::ostream &out) const -> std::ostream & override;
   bool deserialize(std::istream &in) override;
