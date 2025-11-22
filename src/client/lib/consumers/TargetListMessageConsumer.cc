ccf608edd986212b2b8eda71f611be4bb13c06ff
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-11-22 09:42:52 +0100

    feat: Implemented setting target when receiving message

--- src/client/lib/consumers/TargetListMessageConsumer.cc
+++ src/client/lib/consumers/TargetListMessageConsumer.cc
@@ -1,6 +1,5 @@
 
 #include "TargetListMessageConsumer.hh"
-#include "OutpostDataSource.hh"
 #include "TargetListMessage.hh"
 
 namespace pge {
@@ -22,10 +21,44 @@ void TargetListMessageConsumer::onMessageReceived(const bsgo::IMessage &message)
   }
 }
 
-void TargetListMessageConsumer::registerTarget(const bsgo::TargetData & /*data*/) const
+namespace {
+auto tryGetEntityId(const bsgo::DatabaseEntityMapper &entityMapper,
+                    const bsgo::Uuid dbId,
+                    const bsgo::EntityKind kind) -> std::optional<bsgo::Uuid>
 {
-  // TODO: Handle registering the target
-  warn("should register target");
+  switch (kind)
+  {
+    case bsgo::EntityKind::SHIP:
+      return entityMapper.tryGetShipEntityId(dbId);
+    case bsgo::EntityKind::OUTPOST:
+      return entityMapper.tryGetOutpostEntityId(dbId);
+    default:
+      throw std::invalid_argument("Failed to get entity with unsupported kind " + bsgo::str(kind));
+  }
+}
+} // namespace
+
+void TargetListMessageConsumer::registerTarget(const bsgo::TargetData &data) const
+{
+  const auto maybeSourceEntityId = tryGetEntityId(m_entityMapper, data.sourceDbId, data.sourceKind);
+  if (!maybeSourceEntityId)
+  {
+    error("Failed to register target, failed to find source entity: " + bsgo::str(data.sourceKind)
+          + " " + bsgo::str(data.sourceDbId));
+  }
+
+  const auto maybeTargetEntityId = tryGetEntityId(m_entityMapper, data.targetDbId, data.targetKind);
+  if (!maybeTargetEntityId)
+  {
+    error("Failed to register target, failed to find target entity: " + bsgo::str(data.targetDbId)
+          + " " + bsgo::str(data.targetDbId));
+  }
+
+  auto source = m_coordinator->getEntity(*maybeSourceEntityId);
+  source.targetComp().setTarget(*maybeTargetEntityId);
+
+  const auto target = m_coordinator->getEntity(*maybeTargetEntityId);
+  debug("Registering target " + target.str() + " for " + source.str());
 }
 
 } // namespace pge
