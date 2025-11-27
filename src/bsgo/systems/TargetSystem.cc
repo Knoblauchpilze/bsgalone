4ff587a83404afc0e710e05de8bd984e5af71d23
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-11-27 20:38:07 +0100

    feat: TargetSystem publishes message to clear target when needed

--- src/bsgo/systems/TargetSystem.cc
+++ src/bsgo/systems/TargetSystem.cc
@@ -1,6 +1,7 @@
 
 #include "TargetSystem.hh"
 #include "Coordinator.hh"
+#include "TargetMessage.hh"
 
 namespace bsgo {
 namespace {
@@ -26,7 +27,10 @@ void TargetSystem::updateEntity(Entity &entity,
     return;
   }
 
-  clearTargetIfNotReachable(targetComp, coordinator);
+  if (clearTargetIfNotReachable(targetComp, coordinator))
+  {
+    publishTargetMessage(entity);
+  }
 }
 
 namespace {
@@ -63,7 +67,7 @@ bool targetHasDocked(const Entity &target)
 }
 } // namespace
 
-void TargetSystem::clearTargetIfNotReachable(TargetComponent &targetComp,
+bool TargetSystem::clearTargetIfNotReachable(TargetComponent &targetComp,
                                              const Coordinator &coordinator) const
 {
   const auto target = coordinator.getEntity(*targetComp.target());
@@ -71,10 +75,27 @@ void TargetSystem::clearTargetIfNotReachable(TargetComponent &targetComp,
   const auto dead   = targetIsDead(target);
   const auto docked = targetHasDocked(target);
 
-  if (!target.valid() || dead || docked)
+  const auto shouldClear = !target.valid() || dead || docked;
+
+  if (shouldClear)
   {
     targetComp.clearTarget();
   }
+
+  return shouldClear;
+}
+
+void TargetSystem::publishTargetMessage(const Entity &entity) const
+{
+  const auto dummyPosition = Eigen::Vector3f::Zero();
+  TargetData data{
+    .sourceDbId = entity.dbComp().dbId(),
+    .sourceKind = entity.kind->kind(),
+  };
+
+  auto out = std::make_unique<TargetMessage>(data, dummyPosition);
+
+  pushMessage(std::move(out));
 }
 
 } // namespace bsgo
