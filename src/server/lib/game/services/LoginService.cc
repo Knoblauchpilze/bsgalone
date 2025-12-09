a8672ef71fbf0d35caeac4841ccf238c5d96e52e
KnoblauchPilze <prolific-programmer@tuta.com>
2025-12-09 20:31:56 +0100

    feat: Persist game role upon login

--- src/server/lib/game/services/LoginService.cc
+++ src/server/lib/game/services/LoginService.cc
@@ -7,23 +7,29 @@ LoginService::LoginService(const Repositories &repositories)
   : AbstractService("login", repositories)
 {}
 
-auto LoginService::tryLogin(const std::string &name, const std::string &password) const
-  -> std::optional<Uuid>
+auto LoginService::tryLogin(const LoginData &data) const -> std::optional<Uuid>
 {
-  const auto maybeAccount = m_repositories.accountRepository->findOneByName(name);
+  const auto maybeAccount = m_repositories.accountRepository->findOneByName(data.name);
   if (!maybeAccount)
   {
-    warn("No account with name \"" + name + "\"");
+    warn("No account with name \"" + data.name + "\"");
     return {};
   }
-  if (maybeAccount->password != password)
+  if (maybeAccount->password != data.password)
   {
-    warn("Wrong password for player \"" + name + "\"",
-         "Expected " + maybeAccount->password + " but got " + password);
+    warn("Wrong password for player \"" + data.name + "\"",
+         "Expected " + maybeAccount->password + " but got " + data.password);
     return {};
   }
 
   const auto player = m_repositories.playerRepository->findOneByAccount(maybeAccount->id);
+  PlayerRole playerRole{
+    .player = player.id,
+    .role   = data.role,
+  };
+  m_repositories.playerRoleRepository->save(playerRole);
+
+  info("Player " + data.name + " logged in with role " + str(data.role));
 
   return player.id;
 }
