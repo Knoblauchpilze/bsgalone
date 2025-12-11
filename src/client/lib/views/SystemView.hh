df00c2be88b84f296dd2dda0ff02d34dd32de8b9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-12-11 08:10:50 +0100

    feat: System view receives the list of players and exposes it

--- src/client/lib/views/SystemView.hh
+++ src/client/lib/views/SystemView.hh
@@ -4,6 +4,7 @@
 #include "AbstractView.hh"
 #include "Coordinator.hh"
 #include "DatabaseEntityMapper.hh"
+#include "PlayerData.hh"
 #include "Uuid.hh"
 #include <eigen3/Eigen/Eigen>
 #include <memory>
@@ -17,6 +18,7 @@ class SystemView : public AbstractView
   SystemView(bsgo::CoordinatorShPtr coordinator, const bsgo::DatabaseEntityMapper &entityMapper);
   ~SystemView() override = default;
 
+  bool isReady() const noexcept override;
   void reset() override;
 
   auto getAsteroidsWithin(const bsgo::IBoundingBox &bbox) const -> std::vector<bsgo::Entity>;
@@ -25,9 +27,16 @@ class SystemView : public AbstractView
 
   auto getAsteroid(const bsgo::Uuid asteroidDbId) const -> bsgo::Entity;
 
+  auto getSystemPlayers() const -> std::vector<bsgo::PlayerData>;
+
+  protected:
+  void handleMessageInternal(const bsgo::IMessage &message) override;
+
   private:
   bsgo::CoordinatorShPtr m_coordinator{};
   const bsgo::DatabaseEntityMapper &m_entityMapper;
+
+  std::vector<bsgo::PlayerData> m_players{};
 };
 
 using SystemViewShPtr = std::shared_ptr<SystemView>;
