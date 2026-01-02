b6c7ac5af59d971ed88f8956c576afb88c49db98
maxime rigaud <33599677+Knoblauchpilze@users.noreply.github.com>
2026-01-02 21:29:14 +0100

    refactor: Improved test server setup (#72)

---
+++ tests/integration/net/TcpFixture.hh
@@ -0,0 +1,42 @@
+
+#pragma once
+
+#include "SocketShPtr.hh"
+#include <asio.hpp>
+#include <atomic>
+#include <gtest/gtest.h>
+
+namespace test {
+
+class TcpFixture : public ::testing::Test
+{
+  public:
+  TcpFixture();
+  ~TcpFixture() override = default;
+
+  protected:
+  int m_port{};
+  asio::io_context m_context{};
+
+  void SetUp() override;
+  void TearDown() override;
+
+  auto port() const -> int;
+
+  /// @brief - Attempts to synchronously connect to the server available at
+  /// the port defined in the fixture and returns the **client** socket when
+  /// successful. The **server** socket is held by the TCP server and is not
+  /// accessible.
+  /// Note: the server should **already be running**, otherwise this method
+  /// will hang.
+  /// @return - the client socket connected to the server. The socket is in
+  /// a healthy state when returned.
+  auto connectToRunningServer() -> net::SocketShPtr;
+
+  void write(net::SocketShPtr socket, const std::string &data);
+
+  private:
+  static std::atomic_int NEXT_PORT;
+};
+
+} // namespace test
