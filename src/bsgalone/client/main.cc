049d74b4c96c74e4df951009524bbd96d9680b8a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2026-01-17 14:28:49 +0100

    feat: Added experiment to verify new server behavior

---
+++ src/bsgalone/client/main.cc
@@ -0,0 +1,62 @@
+
+#include "App.hh"
+#include "AppDesc.hh"
+#include "CoreException.hh"
+#include "Environment.hh"
+#include "TopViewFrame.hh"
+#include "log/Locator.hh"
+#include "log/PrefixedLogger.hh"
+#include "log/StdLogger.hh"
+
+constexpr auto USERNAME_ENV_KEY  = "USERNAME";
+constexpr auto PASSWORD_ENV_KEY  = "PASSWORD";
+constexpr auto GAME_ROLE_ENV_KEY = "GAMEROLE";
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+  // Create the logger.
+  core::log::StdLogger raw;
+  raw.setLevel(core::log::Severity::DEBUG);
+  core::log::PrefixedLogger logger("server", "main");
+  core::log::Locator::provide(&raw);
+
+  try
+  {
+    logger.notice("Starting application");
+
+    auto tiles  = pge::CenteredViewport({0.0f, 0.0f}, {16.0f, 12.0f});
+    auto pixels = pge::TopLeftViewport({0.0f, 0.0f}, {800.0f, 600.0f});
+
+    pge::AppDesc ad{.dims       = pge::Vec2i{800, 600},
+                    .frame      = std::make_shared<pge::TopViewFrame>(tiles, pixels),
+                    .name       = "bsgalone",
+                    .fixedFrame = true,
+                    .maxFps     = 20};
+
+    const auto port                = core::getPortFromEnvironmentVariable();
+    const auto maybeUserName       = core::tryGetEnvironmentVariable<std::string>(USERNAME_ENV_KEY);
+    const auto maybePassword       = core::tryGetEnvironmentVariable<std::string>(PASSWORD_ENV_KEY);
+    const auto maybeGameRoleString = core::tryGetEnvironmentVariable<std::string>(GAME_ROLE_ENV_KEY);
+
+    bsgalone::client::App demo(ad, port, maybeUserName, maybePassword);
+
+    demo.run();
+  }
+  catch (const core::CoreException &e)
+  {
+    logger.error("Caught internal exception while setting up application", e.what());
+    return EXIT_FAILURE;
+  }
+  catch (const std::exception &e)
+  {
+    logger.error("Caught internal exception while setting up application", e.what());
+    return EXIT_FAILURE;
+  }
+  catch (...)
+  {
+    logger.error("Unexpected error while setting up application");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
