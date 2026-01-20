bbed7baf9eef7970d192c40f00d4a824834d811f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2026-01-20 21:09:28 +0100

    fix: Added more tests for the TcpClient

--- tests/integration/net/client/TcpClientTest.cc
+++ tests/integration/net/client/TcpClientTest.cc
@@ -94,4 +94,48 @@ TEST_F(Integration_Net_Client_TcpClient, PublishesClientDisconnectedEventWhenCli
   EXPECT_EQ(ClientId{34}, event->as<ClientDisconnectedEvent>().clientId());
 }
 
+TEST_F(Integration_Net_Client_TcpClient, SendsDataToServerSocket)
+{
+  auto bus    = std::make_shared<TestEventBus>();
+  auto client = std::make_shared<TcpClient>(bus);
+  client->connect(LOCALHOST_URL, this->port());
+
+  auto sockets = this->waitForServerSocket();
+  sockets.writeServer(ClientId{34});
+
+  auto event = bus->waitForEvent();
+  EXPECT_EQ(EventType::CLIENT_CONNECTED, event->type());
+  EXPECT_EQ(ClientId{34}, event->as<ClientConnectedEvent>().clientId());
+
+  std::string data("test");
+  client->trySend(std::vector<char>(data.begin(), data.end()));
+
+  const auto actual = sockets.readServer(data.size());
+  EXPECT_EQ("test", actual);
+}
+
+TEST_F(Integration_Net_Client_TcpClient, PublishesDataSentEvent)
+{
+  auto bus    = std::make_shared<TestEventBus>();
+  auto client = std::make_shared<TcpClient>(bus);
+  client->connect(LOCALHOST_URL, this->port());
+
+  auto sockets = this->waitForServerSocket();
+  sockets.writeServer(ClientId{34});
+
+  auto event = bus->waitForEvent();
+  EXPECT_EQ(EventType::CLIENT_CONNECTED, event->type());
+  EXPECT_EQ(ClientId{34}, event->as<ClientConnectedEvent>().clientId());
+
+  std::string data("test");
+  const auto expectedMessageId = client->trySend(std::vector<char>(data.begin(), data.end()));
+  EXPECT_TRUE(expectedMessageId.has_value());
+
+  event = bus->waitForEvent();
+  EXPECT_EQ(EventType::DATA_SENT, event->type());
+  EXPECT_EQ(ClientId{34}, event->as<DataSentEvent>().clientId());
+  const std::vector<char> expectedData(data.begin(), data.end());
+  EXPECT_EQ(expectedMessageId.value(), event->as<DataSentEvent>().messageId());
+}
+
 } // namespace net::details
