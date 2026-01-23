50bf5324d078b0aa2d707c02bd93cae584a15343
KnoblauchPilze <prolific-programmer@tuta.com>
2026-01-23 17:26:34 +0100

    feat: Added simplistic GameServer class

--- src/bsgalone/server/main.cc
+++ src/bsgalone/server/main.cc
@@ -1,8 +1,20 @@
 
+#include "Environment.hh"
+#include "GameServer.hh"
 #include "SafetyNet.hh"
 #include "log/Locator.hh"
 #include "log/PrefixedLogger.hh"
 #include "log/StdLogger.hh"
+#include <csignal>
+
+namespace {
+// https://stackoverflow.com/questions/11468414/using-auto-and-lambda-to-handle-signal
+std::function<void(int)> sigIntProcessing;
+void sigIntInterceptor(const int signal)
+{
+  sigIntProcessing(signal);
+}
+} // namespace
 
 int main(int /*argc*/, char ** /*argv*/)
 {
@@ -11,7 +23,14 @@ int main(int /*argc*/, char ** /*argv*/)
   core::log::PrefixedLogger logger("server", "main");
   core::log::Locator::provide(&raw);
 
-  auto gameFunc = []() {};
+  bsgalone::server::GameServer server;
+  const auto port = core::getPortFromEnvironmentVariable();
+
+  sigIntProcessing = [&server](const int /*signal*/) { server.requestStop(); };
+  // https://en.cppreference.com/w/cpp/utility/program/signal
+  std::signal(SIGINT, sigIntInterceptor);
+
+  auto gameFunc = [&server, port]() { server.run(port); };
 
   if (!core::launchProtected(gameFunc, "main", logger))
   {
