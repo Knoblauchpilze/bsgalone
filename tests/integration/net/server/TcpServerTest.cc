35cd672428625fb68876437dc0633fc583763f4b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2026-01-24 16:07:51 +0100

    feat: Server publishes server started/stopped events

--- tests/integration/net/server/TcpServerTest.cc
+++ tests/integration/net/server/TcpServerTest.cc
@@ -12,19 +12,53 @@
 using namespace test;
 using namespace ::testing;
 
-namespace net::details {
+namespace net {
 using Integration_Net_Server_TcpServer = TcpFixture;
+
+namespace {
+void startServer(std::shared_ptr<TcpServer> &server,
+                 const int port,
+                 std::shared_ptr<TestEventBus> &bus)
+{
+  server->start(port);
+
+  const auto actual = bus->waitForEvent();
+  EXPECT_EQ(EventType::SERVER_STARTED, actual->type());
+}
+} // namespace
 
 TEST_F(Integration_Net_Server_TcpServer, ThrowsWhenEventBusIsNull)
 {
   EXPECT_THROW([this]() { TcpServer(nullptr); }(), std::invalid_argument);
 }
 
-TEST_F(Integration_Net_Server_TcpServer, AcceptsConnectionAndPublishesClientConnectedEvent)
+TEST_F(Integration_Net_Server_TcpServer, PublishesServerStartedEvent)
 {
   auto bus    = std::make_shared<TestEventBus>();
   auto server = std::make_shared<TcpServer>(bus);
   server->start(this->port());
+
+  const auto actual = bus->waitForEvent();
+  EXPECT_EQ(EventType::SERVER_STARTED, actual->type());
+}
+
+TEST_F(Integration_Net_Server_TcpServer, PublishesServerStoppedEvent)
+{
+  auto bus    = std::make_shared<TestEventBus>();
+  auto server = std::make_shared<TcpServer>(bus);
+  startServer(server, this->port(), bus);
+
+  server->stop();
+
+  const auto actual = bus->waitForEvent();
+  EXPECT_EQ(EventType::SERVER_STOPPED, actual->type());
+}
+
+TEST_F(Integration_Net_Server_TcpServer, AcceptsConnectionAndPublishesClientConnectedEvent)
+{
+  auto bus    = std::make_shared<TestEventBus>();
+  auto server = std::make_shared<TcpServer>(bus);
+  startServer(server, this->port(), bus);
 
   // The client socket needs to be kept so that the server socket does not
   // detect that it's gone and closes the connection
@@ -42,7 +76,7 @@ TEST_F(Integration_Net_Server_TcpServer, DetectsDisconnectionAndPublishesClientD
 {
   auto bus    = std::make_shared<TestEventBus>();
   auto server = std::make_shared<TcpServer>(bus);
-  server->start(this->port());
+  startServer(server, this->port(), bus);
 
   auto socket = this->connectToRunningServer();
   auto event  = bus->waitForEvent();
@@ -59,7 +93,7 @@ TEST_F(Integration_Net_Server_TcpServer, PublishesClientDisconnectedEventWhenSer
 {
   auto bus    = std::make_shared<TestEventBus>();
   auto server = std::make_shared<TcpServer>(bus);
-  server->start(this->port());
+  startServer(server, this->port(), bus);
 
   auto socket = this->connectToRunningServer();
   auto event  = bus->waitForEvent();
@@ -68,9 +102,9 @@ TEST_F(Integration_Net_Server_TcpServer, PublishesClientDisconnectedEventWhenSer
 
   server->stop();
 
-  event = bus->waitForEvent();
+  event = bus->waitForEvent(EventType::CLIENT_DISCONNECTED);
   EXPECT_EQ(EventType::CLIENT_DISCONNECTED, event->type());
   EXPECT_EQ(expectedClientId, event->as<ClientDisconnectedEvent>().clientId());
 }
 
-} // namespace net::details
+} // namespace net
