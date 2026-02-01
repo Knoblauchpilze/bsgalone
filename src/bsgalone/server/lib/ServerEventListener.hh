cb82939353b9d3e209bbfd4a1398f4512c929573
KnoblauchPilze <prolific-programmer@tuta.com>
2026-02-01 13:47:46 +0100

    feat: Moved ServerEventListener to dedicated class and added tests

---
+++ src/bsgalone/server/lib/ServerEventListener.hh
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "IEventListener.hh"
+#include "ServerStartedEvent.hh"
+#include "ServerStoppedEvent.hh"
+#include <atomic>
+
+namespace bsgalone::server {
+
+class ServerEventListener : public net::IEventListener
+{
+  public:
+  ServerEventListener(std::atomic_bool &started);
+  ~ServerEventListener() = default;
+
+  bool isEventRelevant(const net::EventType &type) const override;
+  void onEventReceived(const net::IEvent &event) override;
+
+  private:
+  std::atomic_bool &m_started;
+
+  void handleServerStarted(const net::ServerStartedEvent & /*event*/);
+  void handleServerStopped(const net::ServerStoppedEvent & /*event*/);
+};
+
+} // namespace bsgalone::server
---
+++ tests/unit/bsgalone/server/lib/ServerEventListenerTest.cc
@@ -0,0 +1,47 @@
+
+#include "ServerEventListener.hh"
+#include "ServerStartedEvent.hh"
+#include "ServerStoppedEvent.hh"
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+namespace bsgalone::server {
+
+TEST(Unit_Bsgalone_Server_ServerEventListenerTest, ConsidersServerStartedEventAsRelevant)
+{
+  std::atomic_bool started;
+  ServerEventListener listener(started);
+  EXPECT_TRUE(listener.isEventRelevant(net::EventType::SERVER_STARTED));
+}
+
+TEST(Unit_Bsgalone_Server_ServerEventListenerTest, ConsidersServerStoppedEventAsRelevant)
+{
+  std::atomic_bool started;
+  ServerEventListener listener(started);
+  EXPECT_TRUE(listener.isEventRelevant(net::EventType::SERVER_STOPPED));
+}
+
+TEST(Unit_Bsgalone_Server_ServerEventListenerTest, SetsStartedToTrueWhenServerStartedEventReceived)
+{
+  std::atomic_bool started(false);
+  ServerEventListener listener(started);
+
+  net::ServerStartedEvent event;
+  listener.onEventReceived(event);
+
+  EXPECT_TRUE(started.load());
+}
+
+TEST(Unit_Bsgalone_Server_ServerEventListenerTest, SetsStartedToFalseWhenServerStoppedEventReceived)
+{
+  std::atomic_bool started(true);
+  ServerEventListener listener(started);
+
+  net::ServerStoppedEvent event;
+  listener.onEventReceived(event);
+
+  EXPECT_FALSE(started.load());
+}
+
+} // namespace bsgalone::server
