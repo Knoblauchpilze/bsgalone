b91e6431c89d8c26057699c6053da89f91e165a2
Knoblauchpilze <prolific-programmer@tuta.com>
2026-05-24 10:41:37 +0200

    feat: Added status UI handler tests

---
+++ tests/unit/bsgalone/client/lib/ui/game/StatusUiHandlerTest.cc
@@ -0,0 +1,84 @@
+
+#include "StatusUiHandler.hh"
+#include "LogoutCommand.hh"
+#include "MockTexturePack.hh"
+#include "TestUiCommandQueue.hh"
+#include "TestUiInputFactory.hh"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace test;
+using namespace ::testing;
+
+namespace bsgalone::client {
+namespace {
+class Unit_Bsgalone_Client_Ui_Game_StatusUiHandler : public Test
+{
+  protected:
+  void SetUp() override
+  {
+    uiCommandQueue = std::make_shared<TestUiCommandQueue>(false);
+    handler        = std::make_unique<StatusUiHandler>(uiCommandQueue);
+  }
+
+  void TearDown() override
+  {
+    // Intentionally empty
+  }
+
+  std::shared_ptr<TestUiCommandQueue> uiCommandQueue{};
+
+  std::unique_ptr<StatusUiHandler> handler{};
+};
+} // namespace
+
+TEST_F(Unit_Bsgalone_Client_Ui_Game_StatusUiHandler, ThrowsWhenCommandQueueIsNull)
+{
+  EXPECT_THROW([this]() { StatusUiHandler(nullptr); }(), std::invalid_argument);
+}
+
+TEST_F(Unit_Bsgalone_Client_Ui_Game_StatusUiHandler, PublishesLogoutCommandWhenLogoutIsConfirmed)
+{
+  MockTexturePack pack;
+  std::cout << "creating handler\n";
+  handler->initializeMenus(pge::Vec2i{1000, 1000}, pack);
+  std::cout << "handler created\n";
+
+  // First click on the logout panel
+  auto data      = generateInputForClickAt(pge::Vec2i{762, 24});
+  auto processed = handler->processUserInput(data);
+
+  // Update the UI to make the menu visible
+  handler->updateUi();
+
+  // Then confirm the logout
+  data      = generateInputForClickAt(pge::Vec2i{415, 559});
+  processed = handler->processUserInput(data);
+
+  EXPECT_TRUE(processed);
+  EXPECT_EQ(1u, uiCommandQueue->messages().size());
+  EXPECT_EQ(UiCommandType::LOGOUT_REQUESTED, uiCommandQueue->messages().at(0)->type());
+}
+
+TEST_F(Unit_Bsgalone_Client_Ui_Game_StatusUiHandler,
+       DoesNotPublishLogoutCommandWhenLogoutIsCancelled)
+{
+  MockTexturePack pack;
+  handler->initializeMenus(pge::Vec2i{1000, 1000}, pack);
+
+  // First click on the logout panel
+  auto data      = generateInputForClickAt(pge::Vec2i{762, 24});
+  auto processed = handler->processUserInput(data);
+
+  // Update the UI to make the menu visible
+  handler->updateUi();
+
+  // Then cancel the logout
+  data      = generateInputForClickAt(pge::Vec2i{597, 500});
+  processed = handler->processUserInput(data);
+
+  EXPECT_TRUE(processed);
+  EXPECT_TRUE(uiCommandQueue->empty());
+}
+
+} // namespace bsgalone::client
